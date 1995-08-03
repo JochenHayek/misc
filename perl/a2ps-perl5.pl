5f3ce5e29c2e447d615bbf279c77569e13cb0b05
Jochen Hayek <Jochen@Hayek.name>
1995-08-03 14:27:08 +0200

    *** empty log message ***

--- perl/a2ps-perl5.pl
+++ perl/a2ps-perl5.pl
@@ -6,7 +6,7 @@
 ;# Software Research Associates, Inc.
 ;# 1-1-1 Hirakawa-cho, Chiyoda-ku, Tokyo 102, Japan
 ;#
-;; $rcsid = q$Id: a2ps-perl5.pl 1.36 1995/06/24 15:13:11 jh Exp $;
+;; $rcsid = q$Id: a2ps-perl5.pl 1.37 1995/08/03 14:27:08 hayek Exp $;
 ;#
 ;# This program is perl version of Miguel Santana's a2ps.  Postscript
 ;# kanji enhancement was done by Naoki Kanazawa <kanazawa@sra.co.jp>.
@@ -253,7 +253,7 @@ sub print_file {
 	}
 	1 while s/\t+/' 'x (length($&) * $tab_w - &pwidth($`) % $tab_w)/e;
 	if ($visualize) {
-	    s/[\200-\377]/'M-'.pack('c',ord($&)&0177)/ge;
+	    #s/[\200-\377]/'M-'.pack('c',ord($&)&0177)/ge;
 	    s/[\000-\007\013\016-\032\034-\037]/'^'.pack('c',ord($&)|0100)/ge;
 	    s/\0177/^?/g;
 	} else {
@@ -618,15 +618,72 @@ ifelse
 /headermargin filenamefontsize 0.25 mul def
 /bodymargin bodyfontsize 0.7 mul def
 
+% use ISO-8859-1 (`Latin1') - taken from i2ps by Gisle Aas
+/NE { %def
+   findfont begin
+      currentdict dup length dict begin
+         { %forall
+            1 index/FID ne {def} {pop pop} ifelse
+         } forall
+         /FontName exch def
+         /Encoding exch def
+         currentdict dup
+      end
+   end
+   /FontName get exch definefont pop
+} bind def
+ISOLatin1Encoding where { pop } { ISOLatin1Encoding
+[/.notdef/.notdef/.notdef/.notdef/.notdef/.notdef/.notdef/.notdef
+/.notdef/.notdef/.notdef/.notdef/.notdef/.notdef/.notdef/.notdef
+/.notdef/.notdef/.notdef/.notdef/.notdef/.notdef/.notdef/.notdef
+/.notdef/.notdef/.notdef/.notdef/.notdef/.notdef/.notdef/.notdef/space
+/exclam/quotedbl/numbersign/dollar/percent/ampersand/quoteright
+/parenleft/parenright/asterisk/plus/comma/minus/period/slash/zero/one
+/two/three/four/five/six/seven/eight/nine/colon/semicolon/less/equal
+/greater/question/at/A/B/C/D/E/F/G/H/I/J/K/L/M/N/O/P/Q/R/S
+/T/U/V/W/X/Y/Z/bracketleft/backslash/bracketright/asciicircum
+/underscore/quoteleft/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s
+/t/u/v/w/x/y/z/braceleft/bar/braceright/asciitilde/.notdef/.notdef
+/.notdef/.notdef/.notdef/.notdef/.notdef/.notdef/.notdef/.notdef
+/.notdef/.notdef/.notdef/.notdef/.notdef/.notdef/.notdef/dotlessi/grave
+/acute/circumflex/tilde/macron/breve/dotaccent/dieresis/.notdef/ring
+/cedilla/.notdef/hungarumlaut/ogonek/caron/space/exclamdown/cent
+/sterling/currency/yen/brokenbar/section/dieresis/copyright/ordfeminine
+/guillemotleft/logicalnot/hyphen/registered/macron/degree/plusminus
+/twosuperior/threesuperior/acute/mu/paragraph/periodcentered/cedilla
+/onesuperior/ordmasculine/guillemotright/onequarter/onehalf/threequarters
+/questiondown/Agrave/Aacute/Acircumflex/Atilde/Adieresis/Aring/AE
+/Ccedilla/Egrave/Eacute/Ecircumflex/Edieresis/Igrave/Iacute/Icircumflex
+/Idieresis/Eth/Ntilde/Ograve/Oacute/Ocircumflex/Otilde/Odieresis
+/multiply/Oslash/Ugrave/Uacute/Ucircumflex/Udieresis/Yacute/Thorn
+/germandbls/agrave/aacute/acircumflex/atilde/adieresis/aring/ae
+/ccedilla/egrave/eacute/ecircumflex/edieresis/igrave/iacute/icircumflex
+/idieresis/eth/ntilde/ograve/oacute/ocircumflex/otilde/odieresis/divide
+/oslash/ugrave/uacute/ucircumflex/udieresis/yacute/thorn/ydieresis]
+def %ISOLatin1Encoding
+} ifelse
+
+ISOLatin1Encoding /Helvetica-ISO            /Helvetica           NE
+ISOLatin1Encoding /Helvetica-Bold-ISO       /Helvetica-Bold      NE
+ISOLatin1Encoding /Courier-ISO              /Courier             NE
+ISOLatin1Encoding /Courier-Bold-ISO         /Courier-Bold        NE
+ISOLatin1Encoding /Courier-BoldOblique-ISO  /Courier-BoldOblique NE
+
+% /Helvetica-ISO            Helvetica           def
+% /Helvetica-Bold-ISO       Helvetica-Bold      def
+% /Courier-ISO              Courier             def
+% /Courier-Bold-ISO         Courier-Bold        def
+% /Courier-BoldOblique-ISO  Courier-BoldOblique def
+
 % Font assignment to differents kinds of "objects"
-/filenamefontname /Helvetica-Bold def
+/filenamefontname /Helvetica-Bold-ISO def
 /stdfilenamefont filenamefontname filenamefontsize getfont def
-/datefont /Helvetica datefontsize getfont def
-/footerfont /Helvetica-Bold datefontsize getfont def
+/datefont   /Helvetica-ISO           datefontsize getfont def
+/footerfont /Helvetica-Bold-ISO      datefontsize getfont def
 /mag { doasciimag { [ 1 0 0 asciimagsize 0 0 ] makefont } if } def
-/bodynfont /Courier bodyfontsize getfont mag def
-/bodybfont /Courier-Bold bodyfontsize getfont mag def
-/bodyofont /Courier-BoldOblique bodyfontsize getfont mag def
+/bodynfont  /Courier-ISO             bodyfontsize getfont mag def
+/bodybfont  /Courier-Bold-ISO        bodyfontsize getfont mag def
+/bodyofont  /Courier-BoldOblique-ISO bodyfontsize getfont mag def
 /fontarray [ bodynfont bodybfont bodyofont ] def
 /bodyfont bodynfont def
 
