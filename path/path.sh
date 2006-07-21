3f660d1b08e47eb0389934e1c3b060596cfd2b83
Jochen Hayek <Jochen@Hayek.name>
2006-07-21 12:44:46 +0200

    *** empty log message ***

--- path/path.sh
+++ path/path.sh
@@ -1,11 +1,11 @@
 :
 
-##time_stamp='Time-stamp: <2003-01-15 20:17:26 johayek>'
-##      rcs_Id='$Id: path.sh 1.7 2003/01/15 19:17:34 johayek Exp $'
+##time_stamp='Time-stamp: <2006-07-21 13:03:40 johayek>'
+##      rcs_Id='$Id: path.sh 1.8 2006/07/21 12:44:46 johayek Exp $'
 ## rcs_RCSfile=$(echo '$RCSfile: path.sh $'|cut -d ' ' -f 2)
 ##  rcs_Source=$(echo '$Source: /Users/johayek/git-servers/github.com/JochenHayek/misc/path/RCS/path.sh $'|cut -d ' ' -f 2)
 ##  rcs_Locker=$(echo '$Locker:  $'|cut -d ' ' -f 2)
-##rcs_Revision=$(echo '$Revision: 1.7 $'|cut -d ' ' -f 2)
+##rcs_Revision=$(echo '$Revision: 1.8 $'|cut -d ' ' -f 2)
 
 path_append()
 {
@@ -17,14 +17,24 @@ path_append()
     esac
 
     export                       PATH=$PATH:${dir}bin
-    test -n "$config" &&
-    export                       PATH=$PATH:${dir}$config
+
+    if test -n "$config"
+    then :
+    	export                   PATH=$PATH:${dir}$config		# long time ago binaries got installed right under $EXEC_PREFIX, i.e. / e.g. /usr/local/sparc-sun-solaris2.7/
+    	export                   PATH=$PATH:${dir}$config/bin		# nowadays, a couple of subdirectories get created below $EXEC_PREFIX, like: bin, lib, libexec, sbin.
+    fi
+
     export                 MANPATH=$MANPATH:${dir}man
     export               INFOPATH=$INFOPATH:${dir}info
 
     if test  "$USER" = 'root' \
        -o "$LOGNAME" = 'root'
     then export                  PATH=$PATH:${dir}sbin
+
+	if test -n "$config"
+	then :
+	    export               PATH=$PATH:${dir}$config/sbin
+	fi
     fi
 
     if test "$(echo ${dir}lib/*.so*)" = "${dir}lib/*.so*"
