b20f2b93243d07b4fd5298d3ac1dae22fcbdfcf5
Jochen Hayek <Jochen@Hayek.name>
2007-04-07 22:54:14 +0200

    *** empty log message ***

--- xml/xml_multi_utility.pl
+++ xml/xml_multi_utility.pl
@@ -1,7 +1,7 @@
 #! /usr/bin/perl -ws
 
-# Time-stamp: <2007-04-07 23:44:11 johayek>
-# $Id: xml_multi_utility.pl 1.2 2007/04/07 21:46:12 johayek Exp $
+# Time-stamp: <2007-04-08 00:53:33 johayek>
+# $Id: xml_multi_utility.pl 1.3 2007/04/07 22:54:14 johayek Exp $
 # $Source: /Users/johayek/git-servers/github.com/JochenHayek/misc/xml/RCS/xml_multi_utility.pl $
 
 # $ ~/Computers/Programming/Languages/Perl/use_XML-Parser.pl -file=...
@@ -10,7 +10,7 @@
   %main::options = ();
 
   $main::options{debug} = 1;
-  $main::options{like_Style_Debug} = 1;
+  $main::options{like_Style_Debug} = 0;
 
   use XML::Parser;
 
@@ -42,19 +42,24 @@
 
       unshift(@{$tree},{});
 
-      &proc_content($tree);
+      &proc_content({ 'content' => $tree
+		    });
     }
 }
 sub proc_content
 {
-  my(@pairs)      = @{$_[0]};
+  my($params) = @_;
+
+  my($return_value);
+
+  my(@pairs) = @{$params->{content}};
   my($ref_attributes) = shift(@pairs);
 
   my(@attributes) = %{$ref_attributes};
 
   if($main::options{like_Style_Debug})
     {
-      printf "=%03d: %s \\\\ (",__LINE__
+      printf STDERR "=%03d: %s \\\\ (",__LINE__
 	,join(' ',@main::stack_of_elements)
 	;
 
@@ -64,7 +69,7 @@ sub proc_content
 	  my($name ) = shift(@attributes);
 	  my($value) = shift(@attributes);
 
-	  print $separator , $name , ' "' , $value , '"';
+	  print STDERR $separator , $name , ' "' , $value , '"';
 
 	  if($separator eq '')
 	    {
@@ -72,8 +77,10 @@ sub proc_content
 	    }
 	}
 
-      printf ")\n";
+      printf STDERR ")\n";
     }
+
+  my($current_pl_dict_key,$current_pl_dict_value);
 
   while($#pairs >= 0)
     {
@@ -93,10 +100,15 @@ sub proc_content
 	    {
 	      if($main::options{like_Style_Debug})
 		{
-		  printf "=%03d: %s || {%s}\n",__LINE__
+		  printf STDERR "=%03d: %s || {%s}\n",__LINE__
 		    ,join(' ',@main::stack_of_elements)
 		    ,$content
 		    ;
+		}
+
+	      if   ($main::stack_of_elements[$#main::stack_of_elements] =~ m/^(key|string|integer)$/)
+		{
+		  $return_value = $content;
 		}
 	    }
 	}
@@ -104,7 +116,61 @@ sub proc_content
 	{
 	  push( @main::stack_of_elements , $tag );
 
-	  &proc_content($content);
+	  my(%this_pl_dict);
+
+	  my($value);
+
+	  if   ($main::stack_of_elements[$#main::stack_of_elements] =~ m/^dict$/)
+	    {
+	      &proc_content({ 'content' => $content
+			    , 'this_pl_dict' => \%this_pl_dict
+			    });
+
+	      if($main::options{debug})
+		{
+		  foreach my $k (sort keys %this_pl_dict)
+		    {
+		      printf STDERR "=%03d: {%s}=>{%s} // %s\n",__LINE__
+			,$k=>$this_pl_dict{$k}
+			,'...'
+			;
+		    }
+		}
+	    }
+	  else
+	    {
+	      $value =
+		  &proc_content({ 'content' => $content
+				});
+	    }
+
+	  if(defined ($value))
+	    {
+	      if   ($main::stack_of_elements[$#main::stack_of_elements] =~ m/^dict$/)
+		{
+		}
+	      elsif($main::stack_of_elements[$#main::stack_of_elements] =~ m/^key$/)
+		{
+		  $current_pl_dict_key = $value;
+
+		  printf STDERR "=%03d: {%s}=>{%s} // %s\n",__LINE__
+		    ,'$current_pl_dict_key'=>$current_pl_dict_key
+		    ,'...'
+		    if $main::options{debug};
+		}
+	      elsif($main::stack_of_elements[$#main::stack_of_elements] =~ m/^(string|integer)$/)
+		{
+		  $current_pl_dict_value = $value;
+
+		  printf STDERR "=%03d: {%s}=>{%s},{%s}=>{%s} // %s\n",__LINE__
+		    ,'$current_pl_dict_key'=>$current_pl_dict_key
+		    ,'$current_pl_dict_value'=>$current_pl_dict_value
+		    ,'...'
+		    if $main::options{debug};
+
+		  $params->{this_pl_dict}{$current_pl_dict_key} = $current_pl_dict_value;
+		}
+	    }
 
 	  pop( @main::stack_of_elements );
 	}
@@ -112,8 +178,10 @@ sub proc_content
 
   if($main::options{like_Style_Debug})
     {
-      printf "=%03d: %s //\n",__LINE__
+      printf STDERR "=%03d: %s //\n",__LINE__
 	,join(' ',@main::stack_of_elements)
 	;
     }
+
+  return $return_value;
 }
