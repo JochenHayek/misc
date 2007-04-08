67ed636215f3c2e2c5322ce10b6c4255caf17b55
Jochen Hayek <Jochen@Hayek.name>
2007-04-08 19:25:48 +0200

    *** empty log message ***

--- xml/xml_multi_utility.pl
+++ xml/xml_multi_utility.pl
@@ -1,25 +1,28 @@
 #! /usr/bin/perl -ws
 
-# Time-stamp: <2007-04-08 02:00:01 johayek>
-# $Id: xml_multi_utility.pl 1.4 2007/04/08 00:01:00 johayek Exp $
+# Time-stamp: <2007-04-08 21:24:18 johayek>
+# $Id: xml_multi_utility.pl 1.5 2007/04/08 19:25:48 johayek Exp $
 # $Source: /Users/johayek/git-servers/github.com/JochenHayek/misc/xml/RCS/xml_multi_utility.pl $
 
-# $ ~/Computers/Programming/Languages/Perl/use_XML-Parser.pl -file=...
+# $ ~/Computers/Data_Formats/Markup_Languages/SGML/PropertyList/use_XML-Parser.pl -file=$HOME/Computers/Data_Formats/Markup_Languages/SGML/PropertyList/membran--chanson--contentsdb.xml
 
 {
+  use XML::Parser;
+
   %main::options = ();
 
   $main::options{debug} = 1;
+
   $main::options{like_Style_Debug} = 0;
 
-  use XML::Parser;
+##$main::options{Style} = 'Debug';
+  $main::options{Style} = 'Tree';
 
-##my($Style) = 'Debug';
-  my($Style) = 'Tree';
+  $main::options{process_PropertyList_p} = 1;
 
   my($tree);
 
-  my($p) = new XML::Parser(Style => $Style);
+  my($p) = new XML::Parser(Style => $main::options{Style});
 
   if   (1)
     {
@@ -30,15 +33,18 @@
     }
   else
     {
+      # this is the example from
+      # $ man XML::Parser::Style::Tree
+
       $tree = $p->parse('<foo><head id="a">Hello <em>there</em></head><bar>Howdy<ref/></bar>do</foo>');
     }
 
-  if   ( $Style eq 'Debug'  )
+  if   ( $main::options{Style} eq 'Debug'  )
     {
     }
-  elsif( $Style eq 'Tree'  )
+  elsif( $main::options{Style} eq 'Tree'  )
     {
-      @main::stack_of_elements = ();
+      @main::stack = ();
 
       unshift(@{$tree},{});
 
@@ -53,36 +59,36 @@ sub proc_content
   my($return_value);
 
   my(@pairs) = @{$params->{content}};
-  my($ref_attributes) = shift(@pairs);
 
-  my(@attributes) = %{$ref_attributes};
+  {
+    my($ref_attributes) = shift(@pairs); # man XML::Parser::Style::Tree : the first item in the array is a (possibly empty) hash reference containing attributes
+    my(@attributes) = %{$ref_attributes};
 
-  if($main::options{like_Style_Debug})
-    {
-      printf STDERR "=%03d: %s \\\\ (",__LINE__
-	,join(' ',@main::stack_of_elements)
-	;
+    if($main::options{like_Style_Debug})
+      {
+	printf STDERR "=%03d: %s \\\\ (",__LINE__
+	  ,join(' ',@main::stack)
+	  ;
 
-      my($separator) = '';
-      while($#attributes >= 0)
-	{
-	  my($name ) = shift(@attributes);
-	  my($value) = shift(@attributes);
+	my($separator) = '';
+	while($#attributes >= 0)
+	  {
+	    my($name ) = shift(@attributes);
+	    my($value) = shift(@attributes);
 
-	  print STDERR $separator , $name , ' "' , $value , '"';
+	    print STDERR $separator , $name , ' "' , $value , '"';
 
-	  if($separator eq '')
-	    {
-	      $separator = ' ';
-	    }
-	}
+	    $separator = ' '
+	      if($separator eq '');
+	  }
 
-      printf STDERR ")\n";
-    }
+	printf STDERR ")\n";
+      }
+  }
 
-  my($current_pl_dict_key,$current_pl_dict_value);
+  my($current_pl_dict_key);
 
-  while($#pairs >= 0)
+  while($#pairs >= 0) # man XML::Parser::Style::Tree : the remainder of the array is a sequence of tag-content pairs representing the content of the element
     {
       my($tag    ) = shift(@pairs);
       my($content) = shift(@pairs);
@@ -91,9 +97,9 @@ sub proc_content
 	{
 	  die '!defined($tag)';
 	}
-      elsif($tag eq '0')
+      elsif($tag eq '0') # man XML::Parser::Style::Tree : text nodes are represented with a pseudo-tag of "0" and the string (aka "PCDATA") is their content
 	{
-	  if( $content =~ m/^\s*$/ ) # this is *not* like Style=>Debug
+	  if( $content =~ m/^\s*$/ ) # as opposed to Style=>Debug, here we don't deal with the occurrence of space as PCDATA
 	    {
 	    }
 	  else
@@ -101,33 +107,57 @@ sub proc_content
 	      if($main::options{like_Style_Debug})
 		{
 		  printf STDERR "=%03d: %s || {%s}\n",__LINE__
-		    ,join(' ',@main::stack_of_elements)
+		    ,join(' ',@main::stack)
 		    ,$content
 		    ;
 		}
 
-	      if   ($main::stack_of_elements[$#main::stack_of_elements] =~ m/^(key|string|integer)$/)
+	      if($main::options{process_PropertyList_p})
 		{
-		  $return_value = $content;
+		  # within PropertyList_s we are only interested in PCDATA as part of a key or of "primitive types"
+
+		  if   ($main::stack[$#main::stack] =~ m/^(key|data|date|real|integer|string)$/)
+		    {
+		      $return_value = $content;
+		    }
+		  else
+		    {
+		      die "\$main::stack[$#main::stack]=>{$main::stack[$#main::stack]},\$content=>{$content}"
+		    }
 		}
 	    }
 	}
-      else
+      elsif($tag ne '0') # man XML::Parser::Style::Tree : for elements, the content is an array reference
 	{
-	  push( @main::stack_of_elements , $tag );
+	  push( @main::stack , $tag );
 
-	  my($value);
+	  my($value);		# we are going to visit "content", and $value is what we will get back from visiting the content during PropertyList processing
 
 	  if   (0)
 	    {
 	    }
-	  elsif($main::stack_of_elements[$#main::stack_of_elements] =~ m/^dict$/)
+	  elsif($main::options{process_PropertyList_p} && ($main::stack[$#main::stack] =~ m/^(true|false)$/))
+	    {
+	      if(0 && $main::options{debug})
+		{
+		  printf STDERR "=%03d: {%s}=>{%s} // %s\n",__LINE__
+		    ,"\$main::stack[$#main::stack]"=>$main::stack[$#main::stack]
+		    ,'...'
+		    ;
+		}
+
+	      $value = $return_value = ($main::stack[$#main::stack] =~ 'true')
+	    }
+	  elsif($main::options{process_PropertyList_p} && ($main::stack[$#main::stack] =~ m/^dict$/))
 	    {
 	      my(%this_pl_dict);
 
 	      &proc_content({ 'content' => $content
 			    , 'this_pl_dict' => \%this_pl_dict
 			    });
+
+	      # we are back from constructing the "dict",
+	      # so now we can have a look at it:
 
 	      if($main::options{debug})
 		{
@@ -138,8 +168,8 @@ sub proc_content
 		      if(ref(\$v) eq 'SCALAR')
 			{
 			  printf STDERR "=%03d: {%s}=>{%s} // %s\n",__LINE__
-			    ,$k=>$this_pl_dict{$k}
-			    ,'...'
+			    ,$k=>$v
+			    ,'another k=>v assoc ...'
 			    ;
 			}
 		      else
@@ -147,15 +177,15 @@ sub proc_content
 			  printf STDERR "=%03d: {%s}=>{%s},{%s}=>{%s} // %s\n",__LINE__
 			    ,"\$this_pl_dict{$k}"=>$this_pl_dict{$k}
 			    ,"ref(\$this_pl_dict{$k})"=>ref($this_pl_dict{$k})
-			    ,'...'
+			    ,'another k=>v assoc ...'
 			    ;
 			}
 		    }
 		}
 
-	      $return_value = \%this_pl_dict;
+	      $value = $return_value = \%this_pl_dict;
 	    }
-	  elsif($main::stack_of_elements[$#main::stack_of_elements] =~ m/^array$/)
+	  elsif($main::options{process_PropertyList_p} && ($main::stack[$#main::stack] =~ m/^array$/))
 	    {
 	      my(@this_pl_array);
 
@@ -164,85 +194,136 @@ sub proc_content
 				, 'this_pl_array' => \@this_pl_array
 				});
 
+	      # we are back from constructing the "array",
+	      # so now we can have a look at it:
+
 	      if($main::options{debug})
 		{
 		  my($i);
 		  for($i=0;$i<=$#this_pl_array;$i++)
 		    {
-		      printf STDERR "=%03d: {%s}=>{%s} // %s\n",__LINE__
-			,"\$this_pl_array[$i]"=>$this_pl_array[$i]
-			,'...'
-			;
+		      my($v) = $this_pl_array[$i];
+
+		      if(ref(\$v) eq 'SCALAR')
+			{
+			  printf STDERR "=%03d: {%s}=>{%s} // %s\n",__LINE__
+			    ,"\$this_pl_array[$i]"=>$this_pl_array[$i]
+			    ,'...'
+			    ;
+			}
+		      else
+			{
+			  printf STDERR "=%03d: {%s}=>{%s},{%s}=>{%s} // %s\n",__LINE__
+			    ,"\$this_pl_array[$i]"=>$this_pl_array[$i]
+			    ,"ref(\$this_pl_array[$i])"=>ref($this_pl_array[$i])
+			    ,'...'
+			    ;
+			}
 		    }
 		}
 
-	      $return_value = \@this_pl_array;
+	      $value = $return_value = \@this_pl_array;
 	    }
 	  else
 	    {
+	      # this *may* still be according to $main::options{process_PropertyList_p}
+	      # or just ordinary expat tree traversal
+
 	      $value =
 		  &proc_content({ 'content' => $content
 				});
 	    }
 
-	  if(defined ($value))
+	  # after returning from visiting content ...
+
+	  if($main::options{process_PropertyList_p})
 	    {
-	      if   (0)
+	      if(!defined ($value))
 		{
+		  die "\$main::stack[$#main::stack]=>{$main::stack[$#main::stack]}"
 		}
-	      elsif($main::stack_of_elements[$#main::stack_of_elements] =~ m/^key$/)
+	      else
 		{
-		  $current_pl_dict_key = $value;
+		  if   (0)
+		    {
+		    }
+		  elsif($main::stack[$#main::stack] =~ m/^key$/)
+		    {
+		      # so that we will keep the name of the key in mind until we will find the value of this key
 
-		  printf STDERR "=%03d: {%s}=>{%s} // %s\n",__LINE__
-		    ,'$current_pl_dict_key'=>$current_pl_dict_key
-		    ,'...'
-		    if $main::options{debug};
-		}
-	      elsif($main::stack_of_elements[$#main::stack_of_elements] =~ m/^(array|dict)$/)
-		{
-		  printf STDERR "=%03d: {%s}=>{%s} // %s\n",__LINE__
-		    ,"\$main::stack_of_elements[$#main::stack_of_elements]"=>$main::stack_of_elements[$#main::stack_of_elements]
-		    ,'...'
-		    if $main::options{debug};
+		      $current_pl_dict_key = $value; # actually $value here is the key *name*
 
-		  $params->{this_pl_dict}{$current_pl_dict_key} = $value;
-		}
-	      elsif($main::stack_of_elements[$#main::stack_of_elements] =~ m/^(string|integer)$/)
-		{
-		  $current_pl_dict_value = $value;
+		      printf STDERR "=%03d: {%s}=>{%s} // %s\n",__LINE__
+			,'$current_pl_dict_key'=>$current_pl_dict_key
+			,'with value'
+			if 0 && $main::options{debug};
+		    }
+		##elsif($main::stack[$#main::stack] =~ m/^(data|date|real|integer|string|true|false)$/)
+		  elsif(1)
+		    {
+		      if ($#main::stack == 0)
+			{
+			  # ???
 
-		  printf STDERR "=%03d: {%s}=>{%s},{%s}=>{%s} // %s\n",__LINE__
-		    ,'$current_pl_dict_key'=>$current_pl_dict_key
-		    ,'$current_pl_dict_value'=>$current_pl_dict_value
-		    ,'...'
-		    if $main::options{debug};
+			  printf STDERR "=%03d: {%s}=>{%s} // %s\n",__LINE__
+			    ,"\$main::stack[$#main::stack]"=>$main::stack[$#main::stack]
+			    ,'$#main::stack == 0 ...'
+			    if $main::options{debug};
+			}
+		      elsif($main::stack[$#main::stack - 1] eq 'plist')
+			{
+			  # ???
 
-		  $params->{this_pl_dict}{$current_pl_dict_key} = $current_pl_dict_value;
+			  printf STDERR "=%03d: {%s}=>{%s},{%s}=>{%s} // %s\n",__LINE__
+			    ,"\$main::stack[$#main::stack]"=>$main::stack[$#main::stack]
+			    ,"\$main::stack[$#main::stack - 1]"=>$main::stack[$#main::stack - 1]
+			    ,'...'
+			    if $main::options{debug};
+			}
+		      elsif($main::stack[$#main::stack - 1] eq 'dict')
+			{
+			  printf STDERR "=%03d: {%s}=>{%s},{%s}=>{%s} // %s\n",__LINE__
+			    ,'$current_pl_dict_key'=>$current_pl_dict_key
+			    ,'$value'=>$value
+			    ,'...'
+			    if 0 && $main::options{debug};
+
+			  $params->{this_pl_dict}{$current_pl_dict_key} = $value;
+			}
+		      elsif($main::stack[$#main::stack - 1] eq 'array')
+			{
+			  printf STDERR "=%03d: {%s}=>{%s} // %s\n",__LINE__
+			    ,'$value'=>$value
+			    ,'pushing a value onto a stack ...'
+			    if $main::options{debug};
+
+			  push( @{$params->{this_pl_array}} , $value );
+			}
+		      else
+			{
+			  die "\$main::stack[$#main::stack - 1]=>{$main::stack[$#main::stack - 1]}"
+			}
+		    }
+		  else
+		    {
+		      die "\$main::stack[$#main::stack]=>{$main::stack[$#main::stack]}"
+		    }
 		}
 	    }
-	  else
-	    {
-	      if   (0)
-		{
-		}
-	      elsif($main::stack_of_elements[$#main::stack_of_elements] =~ m/^(array|dict)$/)
-		{
-		  printf STDERR "=%03d: {%s}=>{%s} // %s\n",__LINE__
-		    ,"\$main::stack_of_elements[$#main::stack_of_elements]"=>$main::stack_of_elements[$#main::stack_of_elements]
-		    ,'...'
-		    if $main::options{debug};
-		}
-	    }
 
-	  pop( @main::stack_of_elements );
+	  pop( @main::stack );
 	}
+      else
+	{
+	  die "\$tag=>{$tag}";
+	}
+
     }
 
   if($main::options{like_Style_Debug})
     {
       printf STDERR "=%03d: %s //\n",__LINE__
-	,join(' ',@main::stack_of_elements)
+	,join(' ',@main::stack)
 	;
     }
 
