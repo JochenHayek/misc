d82f1740138a29f16a61f19998f0a43b8f5367cb
Jochen Hayek <Jochen@Hayek.name>
2007-04-12 17:22:44 +0200

    *** empty log message ***

--- xml/xml_multi_utility.pl
+++ xml/xml_multi_utility.pl
@@ -1,15 +1,15 @@
 #! /usr/bin/perl -w
 
-($emacs_Time_stamp) = 'Time-stamp: <2007-04-12 19:17:59 johayek>' =~ m/<(.*)>/;
+($emacs_Time_stamp) = 'Time-stamp: <2007-04-12 19:22:39 johayek>' =~ m/<(.*)>/;
 
 # Time-stamp: <2007-04-10 16:00:13 johayek>
-# $Id: xml_multi_utility.pl 1.33 2007/04/12 17:18:45 johayek Exp $
+# $Id: xml_multi_utility.pl 1.34 2007/04/12 17:22:44 johayek Exp $
 # $Source: /Users/johayek/git-servers/github.com/JochenHayek/misc/xml/RCS/xml_multi_utility.pl $
 
-          $rcs_Id=(join(' ',((split(/\s/,'$Id: xml_multi_utility.pl 1.33 2007/04/12 17:18:45 johayek Exp $'))[1..6])));
-#	$rcs_Date=(join(' ',((split(/\s/,'$Date: 2007/04/12 17:18:45 $'))[1..2])));
+          $rcs_Id=(join(' ',((split(/\s/,'$Id: xml_multi_utility.pl 1.34 2007/04/12 17:22:44 johayek Exp $'))[1..6])));
+#	$rcs_Date=(join(' ',((split(/\s/,'$Date: 2007/04/12 17:22:44 $'))[1..2])));
 #     $rcs_Author=(join(' ',((split(/\s/,'$Author: johayek $'))[1])));
-#   $rcs_Revision=(join(' ',((split(/\s/,'$Revision: 1.33 $'))[1])));
+#   $rcs_Revision=(join(' ',((split(/\s/,'$Revision: 1.34 $'))[1])));
 #	 $RCSfile=(join(' ',((split(/\s/,'$RCSfile: xml_multi_utility.pl $'))[1])));
 #     $rcs_Source=(join(' ',((split(/\s/,'$Source: /Users/johayek/git-servers/github.com/JochenHayek/misc/xml/RCS/xml_multi_utility.pl $'))[1])));
 
@@ -303,7 +303,7 @@ sub job_run
 
 	      if( defined($test_case->{stdin}{file}) )
 		{
-		  printf "    < '%s' \\\n"
+		  printf "    < %s \\\n"
 		    ,$test_case->{stdin}{file}
 		    ;
 		}
