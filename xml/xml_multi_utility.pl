3645bcb44211087211cc9b0de072e0ce564668df
Jochen Hayek <Jochen@Hayek.name>
2007-04-18 12:49:46 +0200

    *** empty log message ***

--- xml/xml_multi_utility.pl
+++ xml/xml_multi_utility.pl
@@ -1,15 +1,15 @@
 #! /usr/bin/perl -w
 
-($emacs_Time_stamp) = 'Time-stamp: <2007-04-18 14:02:15 johayek>' =~ m/<(.*)>/;
+($emacs_Time_stamp) = 'Time-stamp: <2007-04-18 14:49:45 johayek>' =~ m/<(.*)>/;
 
 # Time-stamp: <2007-04-10 16:00:13 johayek>
-# $Id: xml_multi_utility.pl 1.49 2007/04/18 12:03:48 johayek Exp $
+# $Id: xml_multi_utility.pl 1.50 2007/04/18 12:49:46 johayek Exp $
 # $Source: /Users/johayek/git-servers/github.com/JochenHayek/misc/xml/RCS/xml_multi_utility.pl $
 
-          $rcs_Id=(join(' ',((split(/\s/,'$Id: xml_multi_utility.pl 1.49 2007/04/18 12:03:48 johayek Exp $'))[1..6])));
-#	$rcs_Date=(join(' ',((split(/\s/,'$Date: 2007/04/18 12:03:48 $'))[1..2])));
+          $rcs_Id=(join(' ',((split(/\s/,'$Id: xml_multi_utility.pl 1.50 2007/04/18 12:49:46 johayek Exp $'))[1..6])));
+#	$rcs_Date=(join(' ',((split(/\s/,'$Date: 2007/04/18 12:49:46 $'))[1..2])));
 #     $rcs_Author=(join(' ',((split(/\s/,'$Author: johayek $'))[1])));
-#   $rcs_Revision=(join(' ',((split(/\s/,'$Revision: 1.49 $'))[1])));
+#   $rcs_Revision=(join(' ',((split(/\s/,'$Revision: 1.50 $'))[1])));
 #	 $RCSfile=(join(' ',((split(/\s/,'$RCSfile: xml_multi_utility.pl $'))[1])));
 #     $rcs_Source=(join(' ',((split(/\s/,'$Source: /Users/johayek/git-servers/github.com/JochenHayek/misc/xml/RCS/xml_multi_utility.pl $'))[1])));
 
@@ -546,7 +546,7 @@ sub job_telekom_reo
 
       print <<'header_EOF';
 <?xml version="1.0" encoding="ISO-8859-1"?>
-<!DOCTYPE telekom_rechnung PUBLIC "-//Aleph Soft//DTD Telkom-REO 1.0//EN" "http://www.Aleph-Soft.com/DTDs/Telekom-REO-1.0.dtd">
+<!DOCTYPE telekom_rechnung PUBLIC "-//Aleph Soft//DTD Telekom-REO 1.0//EN" "http://www.Aleph-Soft.com/DTDs/Telekom-REO-1.0.dtd">
 <telekom_rechnung>
 header_EOF
 
