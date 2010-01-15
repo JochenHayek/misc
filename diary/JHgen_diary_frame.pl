ea58d574ec3215852d6fed061cc2708b319c942f
Jochen Hayek <Jochen@Hayek.name>
2010-01-15 13:44:04 +0100

    *** empty log message ***

--- diary/JHgen_diary_frame.pl
+++ diary/JHgen_diary_frame.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# $Id: JHgen_diary_frame.pl 1.17 2008/05/13 10:45:06 johayek Exp $
+# $Id: JHgen_diary_frame.pl 1.18 2010/01/15 13:44:04 johayek Exp $
 # $Source: /Users/johayek/git-servers/github.com/JochenHayek/misc/diary/RCS/JHgen_diary_frame.pl $
 
 # Q: until what year do we run here?
@@ -19,12 +19,14 @@
      ,' Saturday'
      );
 
-  # I could generate a correct 1999 calendar using $cal_week this way,
-  # but I'm still not sure, whether the overall usage of $cal_week is correct this way;
-  my($cal_week);
+  use Date::Calc qw( Week_of_Year );
+
+  # I could generate a correct 1999 calendar using $OLD_cal_week this way,
+  # but I'm still not sure, whether the overall usage of $OLD_cal_week is correct this way;
+  my($OLD_cal_week);
 
   for ( $i=0
-      , $cal_week=1
+      , $OLD_cal_week=1
       , $old_year=-1
       ; ## $i < 978393600
       ; $i += 24*60*60
@@ -32,10 +34,13 @@
     {
       ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = gmtime($i);
 
+      my($cal_week) = Week_of_Year( 1900+$year , 1+$mon , $mday );
+
       printf STDERR "=%d: %04.4d-%02.2d-%02.2d: %s=>%02.2d,%s=>%d\n",__LINE__
 	, 1900 + $year
 	, 1 + $mon
 	, $mday
+      ##,'$OLD_cal_week',$OLD_cal_week
 	,'$cal_week',$cal_week
 	,'$i',$i
 	if $debug;
@@ -49,7 +54,7 @@
 	  , $mday
 	  ;
 
-      last if($date__YYYY_MM_DD gt '2009-99-99'); # "last year"
+      last if($date__YYYY_MM_DD gt '2010-99-99'); # "last year"
 
       print
       ##$i,' ',
@@ -60,11 +65,11 @@
 	{
 	  if ($old_year eq $year)
 	    {
-	      $cal_week++;	# this is actually wrong for the last week of the year ... (really???)
+	      $OLD_cal_week++;	# this is actually wrong for the last week of the year ... (really???)
 	    }
 	  else
 	    {
-	      $cal_week = 1;
+	      $OLD_cal_week = 1;
 	      $old_year = $year;
 	    }
 	  if($debug)
@@ -78,6 +83,7 @@
 		, $week_day_names[$wday]
 		;
 	      printf " (week# %02.2d)"
+	      ##, $OLD_cal_week
 		, $cal_week
 		;
 	      print "\n";
@@ -87,7 +93,7 @@
 	{
 	  if (($mon==0) && ($mday==1))
 	    {
-	      $cal_week = 1;
+	      $OLD_cal_week = 1;
 	      $old_year = $year;
 	      printf "%02.2d %s %d %s"
 		, $mday
@@ -95,7 +101,9 @@
 		, 1900 + $year
 		, $week_day_names[$wday]
 		;
-	      printf " (week# %02.2d) // correct previous week no.!!!"
+	    ##printf " (week# %02.2d) // correct previous week no.!!!"
+	      printf " (week# %02.2d)"
+	      ##, $OLD_cal_week
 		, $cal_week
 		;
 	      print "\n";
