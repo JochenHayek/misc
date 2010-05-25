2ce24d58096e859a5d7db065d9fc3e9943052d36
Jochen Hayek <Jochen@Hayek.name>
2010-05-25 18:59:00 +0200

    *** empty log message ***

--- procmail/procmail-from2diary.pl
+++ procmail/procmail-from2diary.pl
@@ -3,7 +3,7 @@
 {
   while(<>)
     {
-      if(m/^From \s+ (?<from>\S+) \s+ (?<wday>\w+) \s+ (?<month>\w+) \s+ (?<mday>\w+) \s+ (?<time>[\d:]+)/x) # \s+ (?<year>\d+)$/x)
+      if(m/^From \s+ (?<from>\S+) \s+ (?<wday>\w+) \s+ (?<month>\w+) \s+ (?<mday>\w+) \s+ (?<time>[\d:]+) \s+ (?<year>\d+)$/x)
 	{
 	  printf STDERR "=%03.3d,%05.5d: {%s}=>{%s},{%s}=>{%s},{%s}=>{%s},{%s}=>{%s},{%s}=>{%s},{%s}=>{%s} // %s\n",__LINE__,$.
 	    ,'$+{from}',$+{from}
@@ -12,7 +12,46 @@
 	    ,'$+{mday}',$+{mday}
 	    ,'$+{time}',$+{time}
 	    ,'$+{year}',$+{year}
-	    ,'...';
+	    ,'...' if 0;
+
+	  %from_captures = %+;
+	}
+      elsif(m/^ \s+ Subject: \s+ (?<subject>.*) $/x)
+	{
+	  printf STDERR "=%03.3d,%05.5d: {%s}=>{%s},{%s}=>{%s} // %s\n",__LINE__,$.
+	    ,'$+{subject}',$+{subject}
+	    ,'$from_captures{from}',$from_captures{from}
+	    ,'...' if 0;
+
+	  %subject_captures = %+;
+
+	  printf "%s %s %s\n\t%s %s: %s; %s: %s\n"
+	    ,$from_captures{mday}
+	    ,$from_captures{month}
+	    ,$from_captures{year}
+	    ,$from_captures{time}
+	    ,'From',$from_captures{from}
+	    ,'Subject',$subject_captures{subject}
+	    if 0;
+	}
+      elsif(m/^ \s+ Folder: \s+ (?<folder>\S+) \s+ (?<size>\d+) $/x)
+	{
+	  printf STDERR "=%03.3d,%05.5d: {%s}=>{%s},{%s}=>{%s} // %s\n",__LINE__,$.
+	    ,'$+{folder}',$+{folder}
+	    ,'$+{size}',$+{size}
+	    ,'...' if 0;
+
+	  %folder_captures = %+;
+
+	  printf "%s %s %s\n\t%s %s: %s; %s: %s; %s: %s\n"
+	    ,$from_captures{mday}
+	    ,$from_captures{month}
+	    ,$from_captures{year}
+	    ,$from_captures{time}
+	    ,'From',$from_captures{from}
+	    ,'Subject',$subject_captures{subject}
+	    ,'Folder',$folder_captures{folder}
+	    ;
 	}
     }
 }
