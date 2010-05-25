483faae39831dbd0ad879d24d137ce8df5596147
Jochen Hayek <Jochen@Hayek.name>
2010-05-25 18:43:32 +0200

    *** empty log message ***

--- procmail/procmail-from2diary.pl
+++ procmail/procmail-from2diary.pl
@@ -3,10 +3,15 @@
 {
   while(<>)
     {
-      if(m/^From \s+ (?<from>.*) \s+/x)
+      if(m/^From \s+ (?<from>\S+) \s+ (?<wday>\w+) \s+ (?<month>\w+) \s+ (?<mday>\w+) \s+ (?<time>[\d:]+)/x) # \s+ (?<year>\d+)$/x)
 	{
-	  printf STDERR "=%03.3d,%05.5d: {%s}=>{%s} // %s\n",__LINE__,$.
+	  printf STDERR "=%03.3d,%05.5d: {%s}=>{%s},{%s}=>{%s},{%s}=>{%s},{%s}=>{%s},{%s}=>{%s},{%s}=>{%s} // %s\n",__LINE__,$.
 	    ,'$+{from}',$+{from}
+	    ,'$+{wday}',$+{wday}
+	    ,'$+{month}',$+{month}
+	    ,'$+{mday}',$+{mday}
+	    ,'$+{time}',$+{time}
+	    ,'$+{year}',$+{year}
 	    ,'...';
 	}
     }
