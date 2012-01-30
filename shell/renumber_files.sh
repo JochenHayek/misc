c44e4af61ce10741730a62ea0c8a356f40ffa4af
Jochen Hayek <Jochen@Hayek.name>
2012-01-30 13:59:07 +0100

    *** empty log message ***

--- shell/renumber_files.sh
+++ shell/renumber_files.sh
@@ -6,35 +6,23 @@
 
 ################################################################################
 
+dry_run_p=true
+
 new_name="$1"
 
 max=$(( $# - 1 ))
 half_max=$(( max / 2  ))
 rem_max=$(( max % 2  ))
-
-half_max_plus_1=$(( max / 2 + 1 ))
+max_formatted=$( printf "%02.2d" "$max" )
 
 false &&
-printf 1>&2 "=%s,%02.2d: %s=>{%s},%s=>{%s},%s=>{%s},%s=>{%s} // %s\n" "$0" $LINENO \
+printf 1>&2 "=%s,%02.2d: %s=>{%s},%s=>{%s},%s=>{%s} // %s\n" "$0" $LINENO \
   '$new_name' "$new_name" \
   '$max' "$max" \
-  '$half_max' "$half_max" \
-  '$rem_max' "$rem_max" \
+  '$max_formatted' "$max_formatted" \
   '...'
 
-if test "$rem_max" -eq 0
-then :
-else :
-  printf 1>&2 "=%s,%02.2d: %s=>{%s},%s=>{%s},%s=>{%s},%s=>{%s} // %s\n" "$0" $LINENO \
-    '$new_name' "$new_name" \
-    '$max' "$max" \
-    '$half_max' "$half_max" \
-    '$rem_max' "$rem_max" \
-    '$max is not an even number'
- exit 1
-fi
-
-for i in $(seq --format="%02.f" 1 2 "$max")
+for i in $(seq --format="%02.f" "$max")
 do :
   : echo $i
 
@@ -47,24 +35,9 @@ do :
   shift
   current_file="$1"
   extension_of_current_file=${current_file##*.}
-##echo mv "$current_file" "${new_name}.${i}.${extension_of_current_file}"
-  mv      "$current_file" "${new_name}.${i}.${extension_of_current_file}"
-done
 
-for i in $(seq --format="%02.f" "$max" -2 1)
-do :
-  : echo $i
-
-  false &&
-  printf 1>&2 "=%s,%02.2d: %s=>{%s},%s=>{%s} // %s\n" "$0" $LINENO \
-    '$new_name' "$new_name" \
-    '$i' "$i" \
-    '...'
-
-  shift
-  current_file="$1"
-##echo mv "$current_file" "${new_name}.${i}.${extension_of_current_file}"
-  mv      "$current_file" "${new_name}.${i}.${extension_of_current_file}"
+  test "${dry_run_p}" = true  && echo mv "$current_file" "${new_name}.${i}of${max_formatted}.${extension_of_current_file}"
+  test "${dry_run_p}" = false &&      mv "$current_file" "${new_name}.${i}of${max_formatted}.${extension_of_current_file}"
 done
 
 ################################################################################
