cc26ef5cc6ee291c5e985c5f2795f58ca73d80ff
Jochen Hayek <Jochen@Hayek.name>
2012-11-28 13:09:20 +0100

    *** empty log message ***

--- csv/csv_reformat.pl
+++ csv/csv_reformat.pl
@@ -9,6 +9,8 @@
 {
   exists($ENV{SEPARATOR}) || die "!exists(\$ENV{SEPARATOR})";
   
+  binmode( ARGV   , ":encoding(UTF-8)" );
+  binmode( STDIN  , ":encoding(UTF-8)" );
   binmode( STDOUT , ":encoding(UTF-8)" );
 
   while(<>)
