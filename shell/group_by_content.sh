ae25994177660435093d70d0292beaca61566966
Jochen Hayek <Jochen@Hayek.name>
2013-07-05 16:23:03 +0200

    *** empty log message ***

--- shell/group_by_content.sh
+++ shell/group_by_content.sh
@@ -1,3 +1,20 @@
 :
 
-sort | perl -ane 'if($F[0] eq $old_F0) {print $i} else { $old_F0 = $F[0]; print "\n",++$i}; print "\t$_"'
+sort |
+
+perl -ane '
+
+  if($F[0] eq $old_F0)
+    {
+      print $i;
+    }
+  else
+    {
+      $old_F0 = $F[0];
+      print "\n",++$i;
+    }
+
+  print "\t$_"
+
+
+'
