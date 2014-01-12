610406d80acf117d92b892564be82e18ea4930ce
Jochen Hayek <Jochen@Hayek.name>
2014-01-12 14:03:17 +0100

    *** empty log message ***

--- procmail/procmail-from2diary.pl
+++ procmail/procmail-from2diary.pl
@@ -1,9 +1,9 @@
 #! /usr/bin/perl -w
 
-our($emacs_Time_stamp) = 'Time-stamp: <2014-01-12 14:57:49 johayek>' =~ m/<(.*)>/;
+our($emacs_Time_stamp) = 'Time-stamp: <2014-01-12 15:02:05 johayek>' =~ m/<(.*)>/;
 
-##our     $rcs_Id=(join(' ',((split(/\s/,'$Id: procmail-from2diary.pl 1.50 2014/01/12 13:58:55 johayek Exp $'))[1..6])));
-##our   $rcs_Date=(join(' ',((split(/\s/,'$Date: 2014/01/12 13:58:55 $'))[1..2])));
+##our     $rcs_Id=(join(' ',((split(/\s/,'$Id: procmail-from2diary.pl 1.51 2014/01/12 14:03:17 johayek Exp $'))[1..6])));
+##our   $rcs_Date=(join(' ',((split(/\s/,'$Date: 2014/01/12 14:03:17 $'))[1..2])));
 ##our $rcs_Author=(join(' ',((split(/\s/,'$Author: johayek $'))[1])));
 ##our    $RCSfile=(join(' ',((split(/\s/,'$RCSfile: procmail-from2diary.pl $'))[1])));
 ##our $rcs_Source=(join(' ',((split(/\s/,'$Source: /Users/johayek/git-servers/github.com/JochenHayek/misc/procmail/RCS/procmail-from2diary.pl $'))[1])));
@@ -11,7 +11,7 @@ our($emacs_Time_stamp) = 'Time-stamp: <2014-01-12 14:57:49 johayek>' =~ m/<(.*)>
 # read a procmail log file -> LOGFILE
 # create diary entries
 
-# $Id: procmail-from2diary.pl 1.50 2014/01/12 13:58:55 johayek Exp $
+# $Id: procmail-from2diary.pl 1.51 2014/01/12 14:03:17 johayek Exp $
 # $Source: /Users/johayek/git-servers/github.com/JochenHayek/misc/procmail/RCS/procmail-from2diary.pl $
 
 # e-mail subjects with "foreign" characters and .maildelivery resp. procmail-from
@@ -175,7 +175,7 @@ sub job_anon
   printf STDERR ">%s,%d,%s\n",__FILE__,__LINE__,$proc_name
     if 0 && $main::options{debug};
 
-  my(%FROM_captures,%TO_captures,%SUBJECT_captures,
+  my(%FROM_captures,%MSG_TO_captures,%SUBJECT_captures,
      %from_captures,%to_captures,%subject_captures,
      %folder_captures);
 
@@ -208,7 +208,7 @@ sub job_anon
 	    if 0;
 
 	  %FROM_captures = %+;
-	  %TO_captures = ();
+	  %MSG_TO_captures = ();
 	  %SUBJECT_captures = ();
 	}
       elsif(m/^MSG_TO=\{(?<MSG_TO>.*)\}$/x)
@@ -218,7 +218,7 @@ sub job_anon
 	    ,'...'
 	    if 0;
 
-	  %TO_captures = %+;
+	  %MSG_TO_captures = %+;
 	}
       elsif(m/^SUBJECT=\{(?<SUBJECT>.*)\}$/x)
 	{
@@ -282,7 +282,7 @@ sub job_anon
 	  &print_entry(
 	    'ref_last_date'    => \$last_date,
 	    'FROM_captures'    => \%FROM_captures,
-	    'TO_captures'      => \%TO_captures,
+	    'MSG_TO_captures'  => \%MSG_TO_captures,
 	    'SUBJECT_captures' => \%SUBJECT_captures,
 	    'from_captures'    => \%from_captures,
 	    'to_captures'      => \%to_captures,
@@ -291,7 +291,7 @@ sub job_anon
 	    );
 
 	  %FROM_captures    = ();
-	  %TO_captures      = ();
+	  %MSG_TO_captures  = ();
 	  %SUBJECT_captures = ();
 	  %from_captures    = ();
 	  %to_captures      = ();
@@ -313,7 +313,7 @@ sub job_anon
 	  &print_entry(
 	    'ref_last_date'    => \$last_date,
 	    'FROM_captures'    => \%FROM_captures,
-	    'TO_captures'      => \%TO_captures,
+	    'MSG_TO_captures'  => \%MSG_TO_captures,
 	    'SUBJECT_captures' => \%SUBJECT_captures,
 	    'from_captures'    => \%from_captures,
 	    'to_captures'      => \%to_captures,
@@ -322,7 +322,7 @@ sub job_anon
 	    );
 
 	  %FROM_captures    = ();
-	  %TO_captures      = ();
+	  %MSG_TO_captures  = ();
 	  %SUBJECT_captures = ();
 	  %from_captures    = ();
 	  %to_captures      = ();
@@ -382,7 +382,7 @@ sub print_entry
 
   # $param{ref_last_date}
   # $param{FROM_captures}
-  # $param{TO_captures}
+  # $param{MSG_TO_captures}
   # $param{SUBJECT_captures}
   # $param{from_captures}
   # $param{to_captures}
@@ -420,7 +420,7 @@ sub print_entry
 	,                                                          $param{from_captures}{time}
 	, 'From'    =>                                             $param{from_captures}{from}
 	, 'FROM'    => exists($param{FROM_captures}{FROM})       ? $param{FROM_captures}{FROM}       : '{!exists(FROM)}'
-	, 'TO'      => exists($param{TO_captures}{TO})           ? $param{TO_captures}{TO}           : '{!exists(TO)}'
+	, 'TO'      => exists($param{MSG_TO_captures}{MSG_TO})   ? $param{MSG_TO_captures}{MSG_TO}   : '{!exists(MSG_TO)}'
 	, 'Subject' => exists($param{subject_captures}{subject}) ? $param{subject_captures}{subject} : '{!exists(subject)}'
 	, 'SUBJECT' => exists($param{SUBJECT_captures}{SUBJECT}) ? $param{SUBJECT_captures}{SUBJECT} : '{!exists(SUBJECT)}'
 	, 'Folder'  =>                                             $param{folder_captures}{folder}
