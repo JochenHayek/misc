786510c58e0dc4b758c300b56b8f895937f7f5f7
Jochen Hayek <Jochen@Hayek.name>
2014-03-15 23:39:20 +0100

    *** empty log message ***

--- diary/JHgen_diary_frame.pl
+++ diary/JHgen_diary_frame.pl
@@ -1,7 +1,7 @@
 #! /usr/local/perlbrew/perls/stable/bin/perl
 #! /usr/bin/perl
 
-# $Id: JHgen_diary_frame.pl 1.23 2012/08/27 21:00:53 johayek Exp $
+# $Id: JHgen_diary_frame.pl 1.24 2014/03/15 23:39:20 johayek Exp $
 # $Source: /Users/johayek/git-servers/github.com/JochenHayek/misc/diary/RCS/JHgen_diary_frame.pl $
 
 # Q: until what year do we run here?
@@ -55,9 +55,9 @@
 	  , $mday
 	  ;
 
-      next if($date__YYYY_MM_DD lt '2014-00-00'); # "first year"
+      next if($date__YYYY_MM_DD lt '2015-00-00'); # "first year"
 
-      last if($date__YYYY_MM_DD gt '2014-99-99'); # "last year"
+      last if($date__YYYY_MM_DD gt '2015-99-99'); # "last year"
 
       print
       ##$i,' ',
