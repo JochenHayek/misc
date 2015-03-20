9a4c2db2b99af7fc4a7868d5dc1e06535efa1d0d
Jochen Hayek <Jochen@Hayek.name>
2015-03-20 03:05:29 +0100

    *** empty log message ***

--- procmail/procmail-from2diary.pl
+++ procmail/procmail-from2diary.pl
@@ -1,12 +1,12 @@
 #! /usr/bin/perl -w
 
-our($emacs_Time_stamp) = 'Time-stamp: <2015-03-19 20:47:13 johayek>' =~ m/<(.*)>/;
+our($emacs_Time_stamp) = 'Time-stamp: <2015-03-20 04:02:53 johayek>' =~ m/<(.*)>/;
 
-# $Id: procmail-from2diary.pl 1.61 2015/03/19 20:23:42 johayek Exp $ Jochen Hayek
+# $Id: procmail-from2diary.pl 1.62 2015/03/20 03:05:29 johayek Exp $ Jochen Hayek
 # $Source: /Users/johayek/git-servers/github.com/JochenHayek/misc/procmail/RCS/procmail-from2diary.pl $
 
-##our     $rcs_Id=(join(' ',((split(/\s/,'$Id: procmail-from2diary.pl 1.61 2015/03/19 20:23:42 johayek Exp $'))[1..6])));
-##our   $rcs_Date=(join(' ',((split(/\s/,'$Date: 2015/03/19 20:23:42 $'))[1..2])));
+##our     $rcs_Id=(join(' ',((split(/\s/,'$Id: procmail-from2diary.pl 1.62 2015/03/20 03:05:29 johayek Exp $'))[1..6])));
+##our   $rcs_Date=(join(' ',((split(/\s/,'$Date: 2015/03/20 03:05:29 $'))[1..2])));
 ##our $rcs_Author=(join(' ',((split(/\s/,'$Author: johayek $'))[1])));
 ##our    $RCSfile=(join(' ',((split(/\s/,'$RCSfile: procmail-from2diary.pl $'))[1])));
 ##our $rcs_Source=(join(' ',((split(/\s/,'$Source: /Users/johayek/git-servers/github.com/JochenHayek/misc/procmail/RCS/procmail-from2diary.pl $'))[1])));
@@ -570,6 +570,42 @@ sub print_entry
 		    , 'Subject' => $param{subject_captures}{subject}
 		    , '*** WILL BE REMOVED, BECAUSE IT IS A SUBSTRING ***'
 		    if 0;
+		  printf 
+		    "\t\t %s:%s;\n"
+		    , 'SUBJECT' => $param{SUBJECT_captures}{SUBJECT}
+		    ;
+		}
+	    }
+
+	  if($both_still_to_be_printed_p)
+	    {
+	      if( $param{subject_captures}{subject} =~ m/=\?UTF-8\?/i )
+		{
+		  $both_still_to_be_printed_p = 0;
+
+		  printf 
+		    "\t\t %s: %s; // %s\n"
+		    , 'Subject' => $param{subject_captures}{subject}
+		    , '*** WILL BE REMOVED, BECAUSE IT IS IN UTF-8 ***'
+		    if 1;
+		  printf 
+		    "\t\t %s:%s;\n"
+		    , 'SUBJECT' => $param{SUBJECT_captures}{SUBJECT}
+		    ;
+		}
+	    }
+
+	  if($both_still_to_be_printed_p)
+	    {
+	      if( $param{subject_captures}{subject} =~ m/=\?ISO-8859-1\?/i )
+		{
+		  $both_still_to_be_printed_p = 0;
+
+		  printf 
+		    "\t\t %s: %s; // %s\n"
+		    , 'Subject' => $param{subject_captures}{subject}
+		    , '*** WILL BE REMOVED, BECAUSE IT IS IN ISO-8859-1 ***'
+		    if 1;
 		  printf 
 		    "\t\t %s:%s;\n"
 		    , 'SUBJECT' => $param{SUBJECT_captures}{SUBJECT}
