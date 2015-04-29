a22f6984080eb133a310f57bc1fb540e263799b1
Jochen Hayek <Jochen@Hayek.name>
2015-04-29 13:46:30 +0200

    *** empty log message ***

--- csv/csv_extract_section.pl
+++ csv/csv_extract_section.pl
@@ -1,6 +1,6 @@
 #! /usr/bin/perl -sw
 
-# $ ~/Computers/Programming/Languages/Perl/csv_extract_section.pl -section=0 /media/NAS/johayek/_banks/BIC-PBNKDEFF/IBAN-DE81100100100637224104/vouchers--SKR03-1200/999990-000--20150428______--Telekom_Festnetz_BLN-4968976753--period-201505/2015_05_Rechnung_4968976753.csv 
+# $ ~/Computers/Programming/Languages/Perl/csv_extract_section.pl -section=1 /media/NAS/johayek/_banks/BIC-PBNKDEFF/IBAN-DE81100100100637224104/vouchers--SKR03-1200/999990-000--20150428______--Telekom_Festnetz_BLN-4968976753--period-201505/2015_05_Rechnung_4968976753.csv 
 
 # Jochen Hayek defining a CSV file section:
 # a section is delimited by an empty line, start of file or EOF.
