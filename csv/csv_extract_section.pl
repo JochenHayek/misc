8b486c64df82451dee234bfe951346da9b554b09
Jochen Hayek <Jochen@Hayek.name>
2015-04-29 14:53:37 +0200

    *** empty log message ***

--- csv/csv_extract_section.pl
+++ csv/csv_extract_section.pl
@@ -1,6 +1,6 @@
 #! /usr/bin/perl -sw
 
-# $ ~/Computers/Programming/Languages/Perl/csv_extract_section.pl -section=1 /media/NAS/johayek/_banks/BIC-PBNKDEFF/IBAN-DE81100100100637224104/vouchers--SKR03-1200/999990-000--20150428______--Telekom_Festnetz_BLN-4968976753--period-201505/2015_05_Rechnung_4968976753.csv 
+# $ ~/Computers/Programming/Languages/Perl/csv_extract_section.pl -section=1 < /media/NAS/johayek/_banks/BIC-PBNKDEFF/IBAN-DE81100100100637224104/vouchers--SKR03-1200/999990-000--20150428______--Telekom_Festnetz_BLN-4968976753--period-201505/2015_05_Rechnung_4968976753.csv 
 
 # Jochen Hayek defining a CSV file section:
 # a section is delimited by an empty line, start of file or EOF.
@@ -23,6 +23,7 @@ use utf8;
   binmode( STDIN  , ":encoding(UTF-8)" );
   binmode( STDOUT , ":encoding(UTF-8)" );
 
+##while(<>)	# cannot use <>, because "binmode(STDIN,...)" does not apply on it
   while(<STDIN>)
     {
       chomp;
@@ -30,7 +31,7 @@ use utf8;
       printf STDERR "*** %s=>{%s} // %s\n",
         'length($_)' => length($_),
         '...'
-        ;
+        if 0;
 
       if($_ eq '')
     ##if(m/^\s*$/)
