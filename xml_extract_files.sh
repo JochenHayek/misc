fee9b9618a1fa36b4ba8b7f8cde615b8a8e1a0d1
Jochen Hayek <Jochen+github-oss@Hayek.name>
2016-07-04 21:21:50 +0200

    Add files via upload

---
+++ xml_extract_files.sh
@@ -0,0 +1,33 @@
+:
+
+# http://www.gnu.org/software/qexo/XQ-Gen-XML.html
+#
+#   Generate all the HTML output files in a single XQuery run, by putting them in a single large XML object, like this:
+#
+#     <outputs>
+#       <output-file filename="picture1.html">
+#         <html>contents of picture1.html</html>
+#       </output-file>
+#       <output-file filename="picture2.html">
+#         <html>contents of picture2.html</html>
+#       </output-file>
+#     </outputs>
+#
+#   It is then easy to write a post-processor to split this into separate XML files.
+#
+# now here is an implementation of this post-processor usint xmlstarlet
+
+##XML=$HOME/Computers/Software/Operating_Systems/Unix/Shell/xml_extract_files.t/in/0.xml
+
+for XML in "$@"
+do :
+  : echo "*** ${XML}:"
+
+  for fn in $( xml sel -t -v 'outputs/output-file/@filename' -n "${XML}" )
+  do :
+    : echo "*** ${fn}:"
+
+    xml sel -t -v "outputs/output-file[@filename='${fn}']" -n "${XML}" > "${fn}"
+  done
+
+done
