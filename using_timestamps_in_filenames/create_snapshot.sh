ee5ecf9d8405f1c485108b898999f6ab075a772f
Jochen Hayek <Jochen+github-oss@Hayek.name>
2017-01-16 19:57:12 +0100

    Add files via upload

---
+++ using_timestamps_in_filenames/create_snapshot.sh
@@ -0,0 +1,132 @@
+#! /bin/bash
+#! /bin/ksh
+#! /usr/bin/ksh
+
+# $Id: create_snapshot.sh 1.28 2016/12/21 11:04:58 johayek Exp johayek $
+
+#                           /scp:localhost#2222:/home/jochen_hayek/Computers/Software/Operating_Systems/Unix/Shell/create_snapshot.sh
+# /scp:www.b.shuttle.de:/var/www/customers/jh2005/www/Hayek/Jochen/Computers/Software/Operating_Systems/Unix/Shell/create_snapshot.sh
+
+################################################################################
+
+# wishlist:
+# * accept files from STDIN, if none are on the command list
+
+# related tools:
+# -> ~/Computers/Software/Operating_Systems/Unix/Shell/README--diff_and_snapshot.txt
+
+################################################################################
+
+false &&
+printf 1>&2 "=%s,%d: %s=>{%s} // %s\n" $0 $LINENO \
+  '$#' "$#" \
+  '...' \
+  ;
+
+shopt -s extglob
+
+##date=$( date '+%Y%m%d%H%M%S' )
+
+for i
+do :
+  if   test -h "$i"
+  then continue
+  elif test -f "$i"
+  then :
+  else :
+    false &&
+    printf 1>&2 "=%s,%d: %s=>{%s} // %s\n" $0 $LINENO \
+      '$i' "$i" \
+      'does not exist, ignored' \
+      ;
+    continue
+  fi
+
+  false &&
+  printf 1>&2 "=%s,%d: %s=>{%s} // %s\n" $0 $LINENO \
+    '$i' "$i" \
+    '...' \
+    ;
+
+  # you may want to use either of them:
+  # * gmtime
+  # * localtime
+
+  date=$( perl -MFile::stat -MPOSIX -e 'printf "%s\n",( strftime "%Y%m%d%H%M%S",localtime(stat($ARGV[0])->mtime) )' "$i" )
+
+  case "$i" in
+  ##*.~+([0-9])~ | *.~+([0-9]).+([0-9])~ | *.~+([0-9]).+([0-9]).~ )
+    *.~+([[:digit:]])~ | *.~+([[:digit:]]).+([[:digit:]])~ | *.~+([[:digit:]]).+([[:digit:]]).~ )
+      dn=$( dirname  "$i" )
+      bn=$( basename "$i" | perl -ne 's/^(.*)(\.~[\.\d]+\.?~)$/$1/ && print $1,"\n"' )
+      if test -e "$dn/$bn.$date"
+      then :
+	false &&
+	printf 1>&2 "=%s,%d: %s=>{%s},%s=>{%s} // %s\n" $0 $LINENO \
+	  '$i' "$i" \
+	  '$dn/$bn.$date' "$dn/$bn.$date" \
+	  'snapshot already exists, removing ...' \
+	  ;
+	rm -f "$i"
+      else :
+	printf 1>&2 "=%s,%d: %s=>{%s},%s=>{%s} // %s\n" $0 $LINENO \
+	  '$i' "$i" \
+	  '$dn/$bn.$date' "$dn/$bn.$date" \
+	  'renaming from ... to ...' \
+	  ;
+	mv "$i" $dn/$bn.$date
+      fi
+      ;;
+    *~ | *.old | *.bak )
+      dn=$( dirname  "$i" )
+      case "$i" in
+	*~    ) bn=$( basename "$i" '~'    ) ;;
+	*.old ) bn=$( basename "$i" '.old' ) ;;
+	*.bak ) bn=$( basename "$i" '.bak' ) ;;
+      esac
+      if test -e "$dn/$bn.$date"
+      then :
+	false &&
+	printf 1>&2 "=%s,%d: %s=>{%s},%s=>{%s} // %s\n" $0 $LINENO \
+	  '$i' "$i" \
+	  '$dn/$bn.$date' "$dn/$bn.$date" \
+	  'snapshot already exists, removing ...' \
+	  ;
+	rm -f "$i"
+      else :
+	printf 1>&2 "=%s,%d: %s=>{%s},%s=>{%s} // %s\n" $0 $LINENO \
+	  '$i' "$i" \
+	  '$dn/$bn.$date' "$dn/$bn.$date" \
+	  'renaming from ... to ...' \
+	  ;
+	mv "$i" "$dn/$bn.$date"
+      fi
+      ;;
+    *)
+      if test -e "$i.$date"
+      then :
+	false &&
+	printf 1>&2 "=%s,%d: %s=>{%s},%s=>{%s} // %s\n" $0 $LINENO \
+	  '$i' "$i" \
+	  '$i.$date' "$i.$date" \
+	  'snapshot already exists' \
+	  ;
+      else :
+	printf 1>&2 "=%s,%d: %s=>{%s},%s=>{%s} // %s\n" $0 $LINENO \
+	  '$i' "$i" \
+	  '$i.$date' "$i.$date" \
+	  'copying from ... to ...' \
+	  ;
+      ##cp -p         		   		"$i" $i.$date
+      ##cp --preserve 		   		"$i" $i.$date
+      ##cp --preserve=mode,ownership,timestamps "$i" $i.$date
+      ##cp --preserve=ownership,timestamps      "$i" $i.$date	# in the cygwin world this works best, otherwise: cp: preserving permissions for '...': Permission denied
+      	cp -p "$i" "$i.$date"
+      fi
+      ;;
+  esac
+done
+
+# Local variables:
+# coding:utf-8-unix
+# End:
