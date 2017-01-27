79ab26d2e39b82bf303878e4124a9393bad90435
Jochen Hayek <Jochen+github-oss@Hayek.name>
2017-01-27 10:13:58 +0100

    replace uses of "echo" with uses of "printf"

--- using_timestamps_in_filenames/purge_snapshots.sh
+++ using_timestamps_in_filenames/purge_snapshots.sh
@@ -1,13 +1,28 @@
 #! /bin/sh
 
+# Time-stamp: <2017-01-27 10:11:00 jhayek>
+
 # $ ~/bin/purge_snapshots.sh DIR
+
+################################################################################
+
+# a simple shell like the BusyBox ash does not have $LINENO,
+# so we want at least to set it to 0:
+#
+if test -z "$LINENO"
+then LINENO=0
+fi
+
+################################################################################
 
 if test $# -eq 1
 then :
   dir="$1"
 else :
-##echo "*** $0 : \$#=>$#,\$1=>{$1}"
-  echo "*** $0 : \$#=>$# // exiting"
+  printf 1>&2 "=%s,%d: %s=>{%s} // %s\n" $0 $LINENO \
+    '$#' "$#" \
+    'should be 1, exiting' \
+    ;
   exit 1
 fi
 
@@ -15,12 +30,19 @@ $HOME/bin/short_list_of_files_without_timestamp_extension.sh "$dir" |
 
 while read f
 do :
-  echo "*** $0 : \$f=>{$f}"
+  printf 1>&2 "\n"
+  printf 1>&2 "=%s,%d: %s=>{%s} // %s\n" $0 $LINENO \
+    '$f' "$f" \
+    '...' \
+    ;
 
   old=
   for i in ${f}.[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]
   do :
-    echo "*** $0 : \$i=>{$i}"
+    printf 1>&2 "=%s,%d: %s=>{%s} // %s\n" $0 $LINENO \
+      '$i' "$i" \
+      '...' \
+      ;
 
     if test -z "$old"
     then
@@ -32,9 +54,11 @@ do :
     if cmp  -s      "$old" "$i"
     then :
 
+      printf 1>&2 "=%s,%d: %s=>{%s} // %s\n" $0 $LINENO \
+	'$i' "$i" \
+	'going to rm' \
+	;
     ##rm --verbose "$i"
-
-      echo "*** $0 : going to rm {$i}"
       rm "$i"
 
     else :
