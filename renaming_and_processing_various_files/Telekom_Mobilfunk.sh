14279c56b9079f0389090d71a054499f19853f07
Jochen Hayek <Jochen@Hayek.name>
2017-05-09 11:21:13 +0200

    *** empty log message ***

--- renaming_and_processing_various_files/Telekom_Mobilfunk.sh
+++ renaming_and_processing_various_files/Telekom_Mobilfunk.sh
@@ -2,12 +2,52 @@
 
 set -x
 
-invoice_incl_csv=$(echo Rechnung_*.csv)
+################################################################################
 
-invoice_excl_csv=$(basename "${invoice_incl_csv}" .csv)
+# we expect there is exactly one matching
 
-$HOME/Computers/Programming/Languages/Perl/xml_multi_utility.pl --job_t_mobile_reo --xml_file=$HOME/Business/Telecommunications/Carriers/t-mobile.de/CSV-Rechnung.effective-20130722.pl.xml "${invoice_incl_csv}" > ${invoice_incl_csv}.xml.NEW
+for invoice_csv in Rechnung_*.csv
+do :
 
-xml sel -t -v "rechnung/summenteil/untersumme[@bezeichnung='Zu zahlender Betrag ']/@betrag" -n ${invoice_incl_csv}.xml.NEW > ${invoice_incl_csv}.calc.txt
+  invoice_basename=$(basename "${invoice_csv}" .csv)
 
-~/bin/pdf-suggest-rename-versioned.sh "${invoice_excl_csv}.pdf" | fgrep CreationDate | sh -x
+  $HOME/Computers/Programming/Languages/Perl/xml_multi_utility.pl --job_t_mobile_reo --xml_file=$HOME/Business/Telecommunications/Carriers/t-mobile.de/CSV-Rechnung.effective-20130722.pl.xml "${invoice_csv}" > ${invoice_csv}.xml.NEW
+
+  xml sel -t -v "rechnung/summenteil/untersumme[@bezeichnung='Zu zahlender Betrag ']/@betrag" -n ${invoice_csv}.xml.NEW > ${invoice_csv}.calc.txt
+
+done
+
+################################################################################
+
+# we expect there is exactly one matching
+
+for invoice_pdf_orig in Rechnung_*.pdf
+do :
+
+  case "${invoice_pdf_orig}" in
+
+    Rechnung_*.*.pdf )
+      echo 1>&2 "*** $0: {${invoice_pdf_orig}} // do not rename a file, that is already versioned"
+      ;;
+
+    * )
+      ~/bin/pdf-suggest-rename-versioned.sh "${invoice_pdf_orig}" | fgrep CreationDate | sh -x
+      ;;
+
+  esac
+
+done
+
+################################################################################
+
+# we expect there is exactly one matching
+
+for invoice_pdf_versioned in Rechnung_*.*.pdf
+do :
+
+
+  ~/bin/touch_with_filename.sh          "${invoice_pdf_versioned}"
+
+  touch                              -r "${invoice_pdf_versioned}" GesamtEVN_*.csv Rechnung_*.csv* Signatur_*.ads
+
+done
