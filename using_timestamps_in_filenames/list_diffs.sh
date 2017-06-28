81d45fcc461976ed65e3c5145d2fb061916737b6
Jochen Hayek <Jochen+github-oss@Hayek.name>
2017-06-28 14:54:32 +0200

    Create list_diffs.sh

---
+++ using_timestamps_in_filenames/list_diffs.sh
@@ -0,0 +1,22 @@
+:
+
+# 
+
+old=
+for i
+do :
+##echo "*** $0 : \$i=>{$i}"
+
+  if test -z "$old"
+  then
+    old="$i"
+    continue
+  fi
+
+  echo diff "$old" "$i"
+
+  old="$i"
+done
+
+# set-buffer-file-coding-system
+#   iso-latin-1-unix
