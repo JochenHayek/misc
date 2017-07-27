a60e29550804a9f0dda9e1f9515e37f46c4a66dc
Jochen Hayek <Jochen@Hayek.name>
2017-07-27 14:28:40 +0200

    *** empty log message ***

--- renaming_and_processing_various_files/Telekom_Mobilfunk.sh
+++ renaming_and_processing_various_files/Telekom_Mobilfunk.sh
@@ -5,6 +5,8 @@
 
 ################################################################################
 
+xmlstarlet=xml
+
 set -x
 
 ################################################################################
@@ -18,7 +20,7 @@ do :
 
   $HOME/Computers/Programming/Languages/Perl/xml_multi_utility.pl --job_t_mobile_reo --xml_file=$HOME/Business/Telecommunications/Carriers/t-mobile.de/CSV-Rechnung.effective-20130722.pl.xml "${invoice_csv}" > ${invoice_csv}.xml.NEW
 
-  xml sel -t -v "rechnung/summenteil/untersumme[@bezeichnung='Zu zahlender Betrag ']/@betrag" -n ${invoice_csv}.xml.NEW > ${invoice_csv}.calc.txt
+  "${xmlstarlet}" sel -t -v "rechnung/summenteil/untersumme[@bezeichnung='Zu zahlender Betrag ']/@betrag" -n ${invoice_csv}.xml.NEW > ${invoice_csv}.calc.txt
 
 done
 
