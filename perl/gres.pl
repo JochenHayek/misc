b795ba48ceabaab6e8fa15015c98a3b8ee882104
Jochen Hayek <Jochen@Hayek.name>
2017-10-30 11:16:44 +0100

    gres.pl should also work with input from stdin

--- perl/gres.pl
+++ perl/gres.pl
@@ -5,31 +5,41 @@
 $old = shift;
 $new = shift;
 
-while ($file = shift @ARGV)
+if($#ARGV >= 0)
 {
-    if (!rename($file,"${file}~"))
-    {
-	warn "cannot rename `$file'";
-	next;
-    }
+  while ($file = shift @ARGV)
+  {
+      if (!rename($file,"${file}~"))
+      {
+	  warn "cannot rename `$file'";
+	  next;
+      }
 
-    if (! open(fh_old,"${file}~") )
-    {
-	warn "cannot open ${file}~";
-	next;
-    }
+      if (! open(fh_old,"${file}~") )
+      {
+	  warn "cannot open ${file}~";
+	  next;
+      }
 
-    if (! open(fh_new,">$file") )
-    {
-	warn "cannot open $file";
-	close(fh_old);
-	next;
-    }
+      if (! open(fh_new,">$file") )
+      {
+	  warn "cannot open $file";
+	  close(fh_old);
+	  next;
+      }
 
-    while (<fh_old>)
-    {
-	s/${old}/${new}/g;
-        print fh_new;
-    }
+      while (<fh_old>)
+      {
+	  s/${old}/${new}/g;
+	  print fh_new;
+      }
 
+  }
+}
+else
+{
+  while (<>)
+  {
+    s/${old}/${new}/g;
+  }
 }
