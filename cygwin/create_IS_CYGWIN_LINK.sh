20ffb359580592ead6df9c3ea45d0a4d791f344a
Jochen Hayek <jochen.hayek@ext.eex.com>
2017-12-06 20:42:48 +0100

    ...

--- cygwin/create_IS_CYGWIN_LINK.sh
+++ cygwin/create_IS_CYGWIN_LINK.sh
@@ -21,6 +21,44 @@ do :
 
   echo 1>&2 "*** $0: {${l}}"
 
+  readlink "${l}" |
+
+  perl -ne > "${l}.IS_CYGWIN_LINK" '
+
+  if(m/^ (?<rhs>.*)  $/x)
+    {
+      my(%plus) = %+;
+
+      if($plus{rhs} =~ s,^ (\.\./)* git-servers /,\$USERPROFILE/git-servers/,x)
+        {
+        }
+
+      print $plus{rhs},"\n";
+    }
+
+'
+
+  if touch --reference="${l}" "${l}.IS_CYGWIN_LINK"
+  then :
+  else :
+    echo 1>&2 "*** $0: {${l}} : touch : \$?=>$?"
+    rm --verbose "${l}" "${l}.IS_CYGWIN_LINK"
+  fi
+
+done
+
+exit 0
+
+################################################################################
+
+
+find . -maxdepth 1 -type l |
+
+while read l
+do :
+
+  echo 1>&2 "*** $0: {${l}}"
+
   file "${l}" |
 
   perl -ne > "${l}.IS_CYGWIN_LINK" '
