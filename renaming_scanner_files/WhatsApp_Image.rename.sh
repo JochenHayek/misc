63dc6c9a5bc83a45817b6327501227e98ed268fa
Jochen Hayek <Jochen@Hayek.name>
2018-08-21 13:51:17 +0200

    ...

--- renaming_scanner_files/WhatsApp_Image.rename.sh
+++ renaming_scanner_files/WhatsApp_Image.rename.sh
@@ -4,8 +4,14 @@
 
 set -x
 
-~/bin/rename \
+~/bin/rename -v \
   \
-  's/^ WhatsApp.Image. (?<YYYY>....)-(?<mm>..)-(?<dd>..) . at . (?<HH>..)\.(?<MM>..)\.(?<SS>..) \.jpeg $/$+{YYYY}$+{mm}$+{dd}$+{HH}$+{MM}$+{SS}--___.jpg/x' \
+  's/^ WhatsApp \s+ Image \s+ (?<YYYY>....)-(?<mm>..)-(?<dd>..) \s+ at \s+ (?<HH>..)\.(?<MM>..)\.(?<SS>..) \.jpeg $/$+{YYYY}$+{mm}$+{dd}$+{HH}$+{MM}$+{SS}--___.jpg/x' \
+  \
+  WhatsApp?Image*.jpeg
+
+~/bin/rename -v \
+  \
+  's/^ WhatsApp \s+ Image \s+ (?<YYYY>....)-(?<mm>..)-(?<dd>..) \s+ at \s+ (?<HH>..)\.(?<MM>..)\.(?<SS>..) \( (?<no>\d+) \) \.jpeg $/$+{YYYY}$+{mm}$+{dd}$+{HH}$+{MM}$+{SS}--___.$+{no}.jpg/x' \
   \
   WhatsApp?Image*.jpeg
