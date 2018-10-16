11a0932e366736d1f2433db63cea2ef0170504f0
Jochen Hayek <Jochen@Hayek.name>
2018-10-16 22:19:51 +0200

    ...

--- renaming_scanner_files/macOS-Screen_Shot-rename.sh
+++ renaming_scanner_files/macOS-Screen_Shot-rename.sh
@@ -12,4 +12,8 @@ shopt -s nullglob
 
 set -x
 
-~/bin/rename -v 'y/ /_/; s/^ (?<prefix>Screen_Shot|Bildschirmfoto|Photo_on)_ (?<Y>\d{4}) - (?<m>\d{2}) - (?<d>\d{2}) _(at|um)_ (?<H>\d{2}) \. (?<M>\d{2}) \. (?<S>\d{2}) ([ap]m)? \.png$/$+{prefix}.$+{Y}$+{m}$+{d}$+{H}$+{M}$+{S}.png/x' Screen*Shot*.png Bildschirmfoto*.png
+~/bin/rename -v \
+  'y/ /_/; s/^ (?<prefix>Screen_Shot|Bildschirmfoto|Photo_on)_ (?<Y>\d{4}) - (?<m>\d{2}) - (?<d>\d{2}) _(at|um)_ (?<H>\d{1,2}) \. (?<M>\d{2}) \. (?<S>\d{2}) (_[ap]m)? \.png$/$+{prefix}.$+{Y}$+{m}$+{d}$+{H}$+{M}$+{S}.png/ix' \
+  Screen*Shot*.png \
+  Bildschirmfoto*.png \
+  ;
