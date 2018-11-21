06d69198da87a26d764fa7d98ec44c290d596642
Jochen Hayek <Jochen@Hayek.name>
2018-11-21 10:58:08 +0100

    handling .mp4 from now on as well

--- renaming_scanner_files/WhatsApp_Image.rename.sh
+++ renaming_scanner_files/WhatsApp_Image.rename.sh
@@ -1,17 +1,22 @@
 :
 
 # e.g. WhatsApp_Image_2016-12-21_at_13.40.40.jpeg
+# e.g. WhatsApp Video 2018-11-17 at 18.51.30.mp4
 
 set -x
 
 ~/bin/rename -v \
   \
-  's/^ WhatsApp \s+ Image \s+ (?<YYYY>....)-(?<mm>..)-(?<dd>..) \s+ at \s+ (?<HH>..)\.(?<MM>..)\.(?<SS>..) \.jpeg $/$+{YYYY}$+{mm}$+{dd}$+{HH}$+{MM}$+{SS}--___.jpg/x' \
+  's/^ WhatsApp \s+ (Image|Video) \s+ (?<YYYY>....)-(?<mm>..)-(?<dd>..) \s+ at \s+ (?<HH>..)\.(?<MM>..)\.(?<SS>..)                  \.(?<suffix>jpeg|mp4) 
+     $/$+{YYYY}$+{mm}$+{dd}$+{HH}$+{MM}$+{SS}--___.$+{suffix}/x' \
   \
-  WhatsApp?Image*.jpeg
+  WhatsApp?Image*.jpeg \
+  WhatsApp?Video*.mp4
 
 ~/bin/rename -v \
   \
-  's/^ WhatsApp \s+ Image \s+ (?<YYYY>....)-(?<mm>..)-(?<dd>..) \s+ at \s+ (?<HH>..)\.(?<MM>..)\.(?<SS>..) \( (?<no>\d+) \) \.jpeg $/$+{YYYY}$+{mm}$+{dd}$+{HH}$+{MM}$+{SS}--___.$+{no}.jpg/x' \
+  's/^ WhatsApp \s+ (Image|Video) \s+ (?<YYYY>....)-(?<mm>..)-(?<dd>..) \s+ at \s+ (?<HH>..)\.(?<MM>..)\.(?<SS>..) \( (?<no>\d+) \) \.(?<suffix>jpeg|mp4) 
+     $/$+{YYYY}$+{mm}$+{dd}$+{HH}$+{MM}$+{SS}--___.$+{no}.$+{suffix}/x' \
   \
-  WhatsApp?Image*.jpeg
+  WhatsApp?Image*.jpeg \
+  WhatsApp?Video*.mp4
