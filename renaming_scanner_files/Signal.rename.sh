4396730857b49b54b605fd1f520aae7ab4f41992
Jochen Hayek <Jochen@Hayek.name>
2019-03-04 12:05:40 +0100

    ...

---
+++ renaming_scanner_files/Signal.rename.sh
@@ -0,0 +1,14 @@
+:
+
+# e.g. signal-attachment-2019-03-04-120047.jpeg
+
+shopt -s nullglob
+
+set -x
+
+~/bin/rename -v </dev/null \
+  \
+  's/^ signal-attachment- (?<YYYY>....)-(?<mm>..)-(?<dd>..)-(?<HH>..)(?<MM>..)(?<SS>..)                  \.(?<suffix>jpeg) 
+     $/$+{YYYY}$+{mm}$+{dd}$+{HH}$+{MM}$+{SS}--___.$+{suffix}/x' \
+  \
+  signal-attachment-*.jpeg
