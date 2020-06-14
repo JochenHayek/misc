fee66fcfb2bd5c40bd70daef03e473a6641da5de
Jochen Hayek <Jochen@Hayek.name>
2020-06-14 18:55:15 +0200

    ...

--- fetchmail--restart_with_new_logfile.sh
+++ fetchmail--restart_with_new_logfile.sh
@@ -1,13 +1,14 @@
 :
 
+# git-servers/github.com/JochenHayek/misc/fetchmail--restart_with_new_logfile.sh
+
 # * supposed to be run on jhk00-domains@jhk00.hostsharing.net
-# * script "lives"     on jhk00-domains@jhk00.hostsharing.net : $HOME/Computers/Software/Operating_Systems/Unix/Shell/
                                                         log_dir=$HOME/var/log
 # * log files  "live"  on jhk00-domains@jhk00.hostsharing.net :
                                                   fetchmail_log=${log_dir}/fetchmail.log
-# * version controlled on                              HayekY : $HOME/Computers/Software/Operating_Systems/Unix/Shell/
 # * old log files to be archived           :
-                                                    archive_dir=/media/NAS/johayek/ARCHIVE/www.b.shuttle.de-non-dated/var/log
+                                                    archive_dir=/scp:ber.jochen.hayek.name:ARCHIVE/hostsharing-longterm/var/log/
+
 
 ################################################################################
 
