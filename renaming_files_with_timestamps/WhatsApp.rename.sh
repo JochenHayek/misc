c196c2a95de9fa9eb8eda5755a189a25ecd31796
Jochen Hayek <jochen@hayek.name>
2021-10-24 22:08:40 +0200

    *** empty log message ***

--- renaming_files_with_timestamps/WhatsApp.rename.sh
+++ renaming_files_with_timestamps/WhatsApp.rename.sh
@@ -1,5 +1,6 @@
 :
 
+# e.g. WhatsApp_Image_2021-10-23 at 4.38.44 PM.jpeg
 # e.g. WhatsApp_Image_2016-12-21_at_13.40.40.jpeg
 # e.g. WhatsApp Video 2018-11-17 at 18.51.30.mp4
 # e.g. WhatsApp Audio 2018-12-21 at 12.30.30.mp4
@@ -11,15 +12,15 @@ set -x
 
 ~/bin/rename -v </dev/null \
   \
-  's/^ WhatsApp \s+ (Image|Video|Audio|Ptt) \s+ (?<YYYY>....)-(?<mm>..)-(?<dd>..) \s+ at \s+ (?<HH>..)\.(?<MM>..)\.(?<SS>..)                  \.(?<suffix>jpeg|mp4|ogg) 
-     $/999990-000--$+{YYYY}$+{mm}$+{dd}$+{HH}$+{MM}$+{SS}--___.$+{suffix}/x' \
+  's/^ WhatsApp \s+ (Image|Video|Audio|Ptt) \s+ (?<YYYY>....)-(?<mm>..)-(?<dd>..) \s+ at \s+ (?<HH>..?)\.(?<MM>..)\.(?<SS>..) ( \s+ (?<ampm>[AP]M) )? \.(?<suffix>jpeg|mp4|ogg) 
+     $/999990-000--$+{YYYY}$+{mm}$+{dd}$+{HH}$+{MM}$+{SS}$+{ampm}--___.$+{suffix}/x' \
   \
   "$@"
 
 ~/bin/rename -v </dev/null \
   \
-  's/^ WhatsApp \s+ (Image|Video|Audio|Ptt) \s+ (?<YYYY>....)-(?<mm>..)-(?<dd>..) \s+ at \s+ (?<HH>..)\.(?<MM>..)\.(?<SS>..) \s* \( (?<no>\d+) \) \.(?<suffix>jpeg|mp4|ogg) 
-     $/999990-000--$+{YYYY}$+{mm}$+{dd}$+{HH}$+{MM}$+{SS}--___.$+{no}.$+{suffix}/x' \
+  's/^ WhatsApp \s+ (Image|Video|Audio|Ptt) \s+ (?<YYYY>....)-(?<mm>..)-(?<dd>..) \s+ at \s+ (?<HH>..?)\.(?<MM>..)\.(?<SS>..) ( \s+ (?<ampm>[AP]M) )? \s* \( (?<no>\d+) \) \.(?<suffix>jpeg|mp4|ogg) 
+     $/999990-000--$+{YYYY}$+{mm}$+{dd}$+{HH}$+{MM}$+{SS}+{ampm}--___.$+{no}.$+{suffix}/x' \
   \
   "$@"
 
