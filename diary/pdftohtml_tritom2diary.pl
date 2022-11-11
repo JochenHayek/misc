3384e287e22e14cfedd9660e060d5dddadc9ffb0
Jochen Hayek <jhayek@KVBB.de>
2022-11-11 16:22:23 +0100

    *** empty log message ***

--- diary/pdftohtml_tritom2diary.pl
+++ diary/pdftohtml_tritom2diary.pl
@@ -3,9 +3,9 @@
 {
   while(<>)
     {
-      if(m/^ (?<dd>\d\d) \. (?<mm>\d\d) \. (?<YYYY>\d\d\d\d) \. $/x)
+      if(m/> (?<dd>\d\d) \. (?<mm>\d\d) \. (?<YYYY>\d\d\d\d) </x)
 	{
-	  printf "%02.2d %02.2d %04.4d\n"
+	  printf "%02.2d %02.2d %04.4d\n",
 	    $+{dd},
 	    $+{mm},
 	    $+{YYYY},
