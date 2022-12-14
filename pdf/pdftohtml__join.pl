7a38ace51f8f18e834f03902f382e7dca80f4b0e
Jochen Hayek <jhayek@KVBB.de>
2022-12-14 10:29:23 +0100

    *** empty log message ***

--- pdf/pdftohtml__join.pl
+++ pdf/pdftohtml__join.pl
@@ -13,7 +13,7 @@
       # <text  top="0298" left="0160" width="0577" height="0019" font="7">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</text>
 
     ##if( m/ " < (?<prefix>[^>]*) > " , " (?<value>.*) " /x )
-      if( m,^ \s* (?<prefix> <text \s+ top="\d+" \s+ left="\d+" \s+ width="\d+" \s+ height="\d+" \s+ font="\d+"> ) (?<value>.*) </text> $,x )
+      if( m,^ \s* (?<prefix> <text \s+ top="\d+" \s+ left="\d+" \s+ width="\d+" \s+ height="\d+" \s+ font="\d+" \s* > ) (?<value>.*) </text> $,x )
 	{
 	  $prefix = $+{prefix}
 	    unless defined($prefix);
