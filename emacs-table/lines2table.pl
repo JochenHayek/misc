7a99a6f3025c2150c324dbc0ccca778fddd23837
Jochen Hayek <jochen@hayek.name>
2023-01-20 11:13:48 +0100

    *** empty log message ***

--- emacs-table/lines2table.pl
+++ emacs-table/lines2table.pl
@@ -2,6 +2,16 @@
 
 # convert a couple of lines into a basic 1-column emacs table
 
+# actually there is a builtin of the emacs tables feature,
+# that already does the job, and even far more complete:
+
+#   table-capture can be invoked from the menu: Tools → Table → Capture Region
+
+#     (table-capture BEG END &optional COL-DELIM-REGEXP ROW-DELIM-REGEXP JUSTIFY MIN-CELL-WIDTH COLUMNS)
+
+#     COL-DELIM-REGEXP : \|
+#     ROW-DELIM-REGEXP : // use a quoted newline
+
 {
   my(@lines);
   my($max_line_length) = -1;
