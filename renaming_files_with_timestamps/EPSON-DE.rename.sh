04c75cf803a8c9dfd9bf8200d7b9fd073603e3e4
Jochen Hayek <jochen@hayek.name>
2023-11-01 10:11:35 +0100

    *** empty log message ***

---
+++ renaming_files_with_timestamps/EPSON-DE.rename.sh
@@ -0,0 +1,23 @@
+:
+
+# e.g. Epson_30102023164546.pdf
+
+# for use with my Epson MFP
+
+shopt -s nullglob
+
+set -x
+
+~/bin/rename -v </dev/null \
+  \
+  's/^ Epson_ (?<dd>..) (?<mm>..) (?<YYYY>....) (?<HH>..) (?<MM>..)(?<SS>..)                 \.(?<suffix>jpg|pdf) 
+     $/999990-000--$+{YYYY}$+{mm}$+{dd}$+{HH}$+{MM}$+{SS}--___.$+{suffix}/x' \
+  \
+  "$@"
+
+~/bin/rename -v </dev/null \
+  \
+  's/^ Epson_ (?<dd>..) (?<mm>..) (?<YYYY>....) (?<HH>..) (?<MM>..)(?<SS>..) \((?<no>\d+)\)                \.(?<suffix>jpg|pdf) 
+     $/999990-000--$+{YYYY}$+{mm}$+{dd}$+{HH}$+{MM}$+{SS}--___.$+{no}.$+{suffix}/x' \
+  \
+  "$@"
