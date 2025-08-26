9668285903f7af410c3dca4cffcde45f230dafd7
Jochen Hayek <git@jochen.hayek.name>
2025-08-26 16:35:19 +0200

    *** empty log message ***

--- renaming_files_with_timestamps/macOS-Screen_Shot.rename.sh
+++ renaming_files_with_timestamps/macOS-Screen_Shot.rename.sh
@@ -3,6 +3,7 @@
 # before:
 #
 #   Screen Shot 2014-04-15 at 21.45.17pm.png
+#   Screenshot_2024-03-20_at_15.47.20.jpeg
 #
 # after:
 #
@@ -13,7 +14,7 @@ shopt -s nullglob
 set -x
 
 ~/bin/rename -v \
-  'y/ /_/; s/^ (?<prefix>Screen_Shot|Screenshot|Bildschirmfoto|Photo_on)_ (?<Y>\d{4}) - (?<m>\d{2}) - (?<d>\d{2}) _(at|um)_ (?<H>\d{1,2}) \. (?<M>\d{2}) \. (?<S>\d{2}) (_[ap]m)? \.png$/$+{prefix}.$+{Y}$+{m}$+{d}$+{H}$+{M}$+{S}--___.png/ix' \
+  'y/ /_/; s/^ (?<prefix>Screen_Shot|Screenshot|Bildschirmfoto|Photo_on)_ (?<Y>\d{4}) - (?<m>\d{2}) - (?<d>\d{2}) _(at|um)_ (?<H>\d{1,2}) \. (?<M>\d{2}) \. (?<S>\d{2}) (_[ap]m)? \. (?<suffix>\w+) $/$+{prefix}.$+{Y}$+{m}$+{d}$+{H}$+{M}$+{S}--___.$+{suffix}/ix' \
   \
   "$@"
 
