382bf32a84b47ad2f0e8076f06988c22fec88eb1
Jochen Hayek <git@jochen.hayek.name>
2025-09-06 18:24:51 +0200

    *** empty log message ***

---
+++ pdf/pdftohtml__adapt_top.pl
@@ -0,0 +1,71 @@
+#! /usr/bin/perl -w
+
+# adapted from pdftohtml__postprocess.pl
+
+################################################################################
+
+{
+  my($last_top) = 0;
+
+  while(<>)
+    {
+      if(m/^  (?<leading_space>\s*)
+
+	    <   (?<text_or_image>text|image)
+	    \s+	top="(?<top>-?\d+)" 
+	    \s+   left="(?<left>-?\d+)" 
+	    \s+  width="(?<width>-?\d+)" 
+	    \s+ height="(?<height>-?\d+)" 
+
+	  /x)
+	{
+	  my(%plus) = %+;
+
+	  $current_top = $plus{top};
+	  $current_top =~ s/^\s*//;
+
+	  if($current_top - $last_top <= 1)		# maybe it should be 2 or 3 instead of 1
+	    {
+	      $current_top = $last_top;
+	    }
+
+	  s/^  (?<leading_space>\s*)
+
+	     <   (?<text_or_image>text|image)
+	     \s+	top="(?<top>-?\d+)" 
+	     \s+   left="(?<left>-?\d+)" 
+	     \s+  width="(?<width>-?\d+)" 
+	     \s+ height="(?<height>-?\d+)" 
+
+	   /
+
+	     sprintf "%s<%-5s %s=\"%04.4d\" %s=\"%04.4d\" %s=\"%04.4d\" %s=\"%04.4d\"",
+
+			     $plus{leading_space} ,
+
+			     $plus{text_or_image},
+
+	       'top'    =>	 $current_top    ,
+
+	       'left'   =>	 $plus{left}   ,
+
+	       'width'  =>	 $plus{width}  ,
+
+	       'height' =>	 $plus{height} ,
+	       ;
+
+	   /ex;
+
+	  print;
+
+	  $last_top = $current_top;
+
+	}
+      else
+	{
+	  print;
+	}
+    }
+}
+
+exit 0;
