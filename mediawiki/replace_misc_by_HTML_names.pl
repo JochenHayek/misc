6485866e4590be33725687b44f5c8c71f8646ea8
Jochen Hayek <git@jochen.hayek.name>
2025-11-07 23:51:16 +0100

    *** empty log message ***

--- mediawiki/replace_misc_by_HTML_names.pl
+++ mediawiki/replace_misc_by_HTML_names.pl
@@ -1,6 +1,11 @@
 #! /usr/bin/perl -i~ -p
 
-s/\[/\&lbrack;/g;
-s/\]/\&rbrack;/g;
+s/\[/\&lbrack;/g;		# "["
+s/\]/\&rbrack;/g;		# "]"
+
+##s/\{/\&lcubk;/g;		# "{"
+##s/\}/\&rcub;/g;		# "}"
+
 s/#/\&#35;/g;		# any nicer replacement?
+
 s/\|/{{!}}/g;
