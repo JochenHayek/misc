2c6c6eaf0f117ee27ddcdac66dac9b6847fe682c
Jochen Hayek <git@jochen.hayek.name>
2026-02-17 17:19:06 +0100

    *** empty log message ***

--- shell/create_vcf.sh
+++ shell/create_vcf.sh
@@ -1,7 +1,7 @@
 :
 
-# emacs-shell $ FIRST_NAME='___' LAST_NAME='___' TOWN='___' COUNTRY='Germany' EMAIL_ADDRESS='___' PHONE_NUMBE='+49___' ~/git-servers/github.com/JochenHayek/misc/shell/create_vcf.sh | tee ~/transfer--DEFINITELY_NON-PERMANENT/___.vcf
-# emacs-shell $ FIRST_NAME='___' LAST_NAME='___' TOWN='___' COUNTRY='Germany' EMAIL_ADDRESS='___' PHONE_NUMBE='+49___' ~/bin/create_vcf.sh | tee ~/transfer--DEFINITELY_NON-PERMANENT/___.vcf
+# emacs-shell $ FIRST_NAME='___' LAST_NAME='___' TOWN='___' COUNTRY='Germany' EMAIL_ADDRESS='___' ORG='___' PHONE_NUMBE='+49___' ~/git-servers/github.com/JochenHayek/misc/shell/create_vcf.sh | tee ~/transfer--DEFINITELY_NON-PERMANENT/___.vcf
+# emacs-shell $ FIRST_NAME='___' LAST_NAME='___' TOWN='___' COUNTRY='Germany' EMAIL_ADDRESS='___' ORG='___' PHONE_NUMBE='+49___' ~/bin/create_vcf.sh | tee ~/transfer--DEFINITELY_NON-PERMANENT/___.vcf
 
 # Q: why run it in an emacs shell?
 # A: very likely one of the names includes an Umlaut or alike, and bash within the usual terminal bash does not deal with it properly.
