5c743327d9c7fa910235676e56da60eaa21caebc
Jochen Hayek <git@jochen.hayek.name>
2026-02-17 16:56:43 +0100

    *** empty log message ***

--- shell/create_vcf.sh
+++ shell/create_vcf.sh
@@ -1,7 +1,12 @@
 :
 
-# $ FIRST_NAME='___' LAST_NAME='___' TOWN='___' COUNTRY='Germany' EMAIL_ADDRESS='___' PHONE_NUMBE='+49___' ~/git-servers/github.com/JochenHayek/misc/shell/create_vcf.sh | tee ~/transfer--DEFINITELY_NON-PERMANENT/___.vcf
-# $ FIRST_NAME='___' LAST_NAME='___' TOWN='___' COUNTRY='Germany' EMAIL_ADDRESS='___' PHONE_NUMBE='+49___' ~/bin/create_vcf.sh | tee ~/transfer--DEFINITELY_NON-PERMANENT/___.vcf
+# emacs-shell $ FIRST_NAME='___' LAST_NAME='___' TOWN='___' COUNTRY='Germany' EMAIL_ADDRESS='___' PHONE_NUMBE='+49___' ~/git-servers/github.com/JochenHayek/misc/shell/create_vcf.sh | tee ~/transfer--DEFINITELY_NON-PERMANENT/___.vcf
+# emacs-shell $ FIRST_NAME='___' LAST_NAME='___' TOWN='___' COUNTRY='Germany' EMAIL_ADDRESS='___' PHONE_NUMBE='+49___' ~/bin/create_vcf.sh | tee ~/transfer--DEFINITELY_NON-PERMANENT/___.vcf
+
+# Q: why run it in an emacs shell?
+# A: very likely one of the names includes an Umlaut or alike, and bash within the usual terminal bash does not deal with it properly.
+
+################################################################################
 
 cat <<EOF
 BEGIN:VCARD
